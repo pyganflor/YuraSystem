24a15990e6918ea43142c4000dddc24dbaf8b2d2
Rafael <prats@pyganflor.com>
2018-12-26 10:57:02 -0500

    Update Rafael

--- app/Modelos/Variedad.php
+++ app/Modelos/Variedad.php
@@ -29,4 +29,30 @@ class Variedad extends Model
     {
         return $this->belongsTo('\yura\Modelos\Planta', 'id_planta');
     }
+
+    public function getDisponiblesToFecha($fecha)
+    {
+        $r = 0;
+        $l = StockApertura::All()->where('estado', '=', 1)->where('disponibilidad', '=', 1)
+            ->where('id_variedad', '=', $this->id_variedad);
+        foreach ($l as $item) {
+            if (opDiasFecha('+', $item->dias, $item->fecha_inicio) == $fecha) {
+                $r += $item->cantidad_disponible;
+            }
+        }
+
+        return $r;
+    }
+
+    public function getPedidosToFecha($fecha)
+    {
+        $r = 0;
+        $l = getResumenPedidosByFecha($fecha, $this->id_variedad);
+        foreach ($l as $item) {
+            dd($item);
+            $ramo = ClasificacionRamo::find($item->id_clasificacion_ramo);
+            $estandar = getCalibreRamoEstandar();
+            $conversion = round(($ramo->nombre / $estandar->nombre) * $item->cantidad, 2);
+        }
+    }
 }
--- resources/views/adminlte/gestion/postcocecha/pedidos/forms/paritals/saldos.blade.php
+++ resources/views/adminlte/gestion/postcocecha/pedidos/forms/paritals/saldos.blade.php
@@ -1,43 +1,60 @@
-<table class="table table-responsive table-bordered" width="100%"
-       style="border: 2px solid #9d9d9d; font-size: 0.8em;">
-    <tr>
-        <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d">
-            Variedades
-        </th>
-        @foreach(getVariedades() as $variedad)
-            <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d">
-                {{$variedad->nombre}}
+<div style="overflow-x: scroll; max-width: 100%">
+    <table class="table table-responsive table-bordered" width="100%"
+           style="border: 2px solid #9d9d9d; font-size: 0.8em;">
+        <tr>
+            <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d"
+                width="10%">
+                Fecha
             </th>
+            <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d"
+                width="15%">
+                Variedades
+            </th>
+            @foreach(getVariedades() as $variedad)
+                <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d"
+                    width="{{75/count(getVariedades())}}%">
+                    {{$variedad->nombre}}
+                </th>
+            @endforeach
+        </tr>
+        @foreach($fechas as $item)
+            <tr>
+                <th class="text-center"
+                    style="border-color: #9d9d9d; background-color: {{$item == $fecha ? '#b9ffb4' : '#e9ecef'}}; border-bottom: 3px solid #9d9d9d; text-align:center"
+                    rowspan="4">
+                    {{$item}}
+                </th>
+            </tr>
+            <tr>
+                <th class="text-center" style="border-color: #9d9d9d; background-color: #add8e6">
+                    Disponibles
+                </th>
+                @foreach(getVariedades() as $variedad)
+                    <td class="text-center" style="border-color: #9d9d9d">
+                        {{$variedad->getDisponiblesToFecha($item)}}
+                    </td>
+                @endforeach
+            </tr>
+            <tr>
+                <th class="text-center" style="border-color: #9d9d9d; background-color: #add8e6">
+                    Pedidos
+                </th>
+                @foreach(getVariedades() as $variedad)
+                    <td class="text-center" style="border-color: #9d9d9d">
+                        ----
+                    </td>
+                @endforeach
+            </tr>
+            <tr style="border-bottom: 3px solid #9d9d9d">
+                <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
+                    Saldo
+                </th>
+                @foreach(getVariedades() as $variedad)
+                    <td class="text-center" style="border-color: #9d9d9d">
+                        ----
+                    </td>
+                @endforeach
+            </tr>
         @endforeach
-    </tr>
-    <tr>
-        <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d">
-            Disponibles
-        </th>
-        @foreach(getVariedades() as $variedad)
-            <td class="text-center" style="border-color: #9d9d9d">
-                {{$variedad->nombre}}
-            </td>
-        @endforeach
-    </tr>
-    <tr>
-        <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d">
-            Pedidos
-        </th>
-        @foreach(getVariedades() as $variedad)
-            <td class="text-center" style="border-color: #9d9d9d">
-                {{$variedad->nombre}}
-            </td>
-        @endforeach
-    </tr>
-    <tr>
-        <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d">
-            Saldo
-        </th>
-        @foreach(getVariedades() as $variedad)
-            <td class="text-center" style="border-color: #9d9d9d">
-                {{$variedad->nombre}}
-            </td>
-        @endforeach
-    </tr>
-</table>
+    </table>
+</div>
