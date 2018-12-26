30f93d3650ee314142b6327be671ce450c0b098b
Rafael <prats@pyganflor.com>
2018-12-26 12:39:46 -0500

    Update Rafael

--- app/Modelos/Variedad.php
+++ app/Modelos/Variedad.php
@@ -33,15 +33,20 @@ class Variedad extends Model
     public function getDisponiblesToFecha($fecha)
     {
         $r = 0;
+        $r_a = 0;   // acumulado
         $l = StockApertura::All()->where('estado', '=', 1)->where('disponibilidad', '=', 1)
             ->where('id_variedad', '=', $this->id_variedad);
         foreach ($l as $item) {
             if (opDiasFecha('+', $item->dias, $item->fecha_inicio) == $fecha) {
-                $r += $item->cantidad_disponible;
+                $r += $item->calcularDisponibles()['estandar'];
+                $r_a += $item->getDisponibles('estandar');
             }
         }
 
-        return $r;
+        return [
+            'cosechado' => $r,
+            'acumulado' => $r_a,
+        ];
     }
 
     public function getPedidosToFecha($fecha)
@@ -49,10 +54,18 @@ class Variedad extends Model
         $r = 0;
         $l = getResumenPedidosByFecha($fecha, $this->id_variedad);
         foreach ($l as $item) {
-            dd($item);
             $ramo = ClasificacionRamo::find($item->id_clasificacion_ramo);
             $estandar = getCalibreRamoEstandar();
             $conversion = round(($ramo->nombre / $estandar->nombre) * $item->cantidad, 2);
+            $r += $conversion;
         }
+        $l = getResumenPedidosByFechaOfTallos($fecha, $this->id_variedad);
+        foreach ($l as $item) {
+            $ramo = ClasificacionRamo::find($item->id_clasificacion_ramo);
+            $estandar = getCalibreRamoEstandar();
+            $conversion = round(($ramo->nombre / $estandar->nombre) * $item->cantidad, 2);
+            $r += $conversion;
+        }
+        return $r;
     }
 }
--- resources/views/adminlte/gestion/postcocecha/pedidos/forms/paritals/saldos.blade.php
+++ resources/views/adminlte/gestion/postcocecha/pedidos/forms/paritals/saldos.blade.php
@@ -2,18 +2,24 @@
     <table class="table table-responsive table-bordered" width="100%"
            style="border: 2px solid #9d9d9d; font-size: 0.8em;">
         <tr>
-            <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d"
-                width="10%">
+            <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: white"
+                width="15%" rowspan="2" colspan="2">
                 Fecha
             </th>
-            <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d"
-                width="15%">
-                Variedades
-            </th>
             @foreach(getVariedades() as $variedad)
-                <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: #9d9d9d"
-                    width="{{75/count(getVariedades())}}%">
+                <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: white"
+                    width="{{75/count(getVariedades())}}%" colspan="2">
                     {{$variedad->nombre}}
+                </th>
+            @endforeach
+        </tr>
+        <tr>
+            @foreach(getVariedades() as $variedad)
+                <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: white">
+                    Cosechado
+                </th>
+                <th class="text-center table-{{getUsuario(Session::get('id_usuario'))->configuracion->skin}}" style="border-color: white">
+                    Acumulado
                 </th>
             @endforeach
         </tr>
@@ -22,7 +28,7 @@
                 <th class="text-center"
                     style="border-color: #9d9d9d; background-color: {{$item == $fecha ? '#b9ffb4' : '#e9ecef'}}; border-bottom: 3px solid #9d9d9d; text-align:center"
                     rowspan="4">
-                    {{$item}}
+                    {{convertDateToText($item)}}
                 </th>
             </tr>
             <tr>
@@ -30,8 +36,11 @@
                     Disponibles
                 </th>
                 @foreach(getVariedades() as $variedad)
-                    <td class="text-center" style="border-color: #9d9d9d">
-                        {{$variedad->getDisponiblesToFecha($item)}}
+                    <td class="text-center" style="border-color: #9d9d9d" title="Cosechado">
+                        {{$variedad->getDisponiblesToFecha($item)['cosechado']}}
+                    </td>
+                    <td class="text-center" style="border-color: #9d9d9d" title="Acumulado">
+                        {{$variedad->getDisponiblesToFecha($item)['acumulado']}}
                     </td>
                 @endforeach
             </tr>
@@ -40,16 +49,19 @@
                     Pedidos
                 </th>
                 @foreach(getVariedades() as $variedad)
-                    <td class="text-center" style="border-color: #9d9d9d">
-                        ----
+                    <td class="text-center" style="border-color: #9d9d9d" title="Cosechado" colspan="2">
+                        {{$variedad->getPedidosToFecha($item)}}
                     </td>
                 @endforeach
             </tr>
-            <tr style="border-bottom: 3px solid #9d9d9d">
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
+            <tr style="border-bottom: 3px solid #9d9d9d; background-color: #e9ecef">
+                <th class="text-center" style="border-color: #9d9d9d">
                     Saldo
                 </th>
                 @foreach(getVariedades() as $variedad)
+                    <td class="text-center" style="border-color: #9d9d9d">
+                        ----
+                    </td>
                     <td class="text-center" style="border-color: #9d9d9d">
                         ----
                     </td>
