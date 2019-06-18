de0bbfa941ed130157d0de2c3646f9524ff57e15
Obrian Villasmil <obrian@pyganflor.com>
2019-06-18 14:25:57 +0000

    updates

--- app/Http/Controllers/FueController.php
+++ app/Http/Controllers/FueController.php
@@ -139,7 +139,7 @@ class FueController extends Controller
         if($request->get('desde') != null && $request->get('hasta') != null)
             $data->whereBetween('comprobante.fecha_emision',[$request->get('desde'),$request->get('hasta')]);
 
-        dd($data->select('comprobante.*','e.*','p.*','dc.nombre','c.id_cliente')->get());
+//        dd($data->select('comprobante.*','e.*','p.*','dc.nombre','c.id_cliente')->get());
         if($excel){
             return $data->select('comprobante.*','e.*','p.*','dc.nombre','c.id_cliente')->get();
         }else{
