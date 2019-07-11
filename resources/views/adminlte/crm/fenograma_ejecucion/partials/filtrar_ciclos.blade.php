336cb8905c399e3ed812988da4ec85261c6a8e85
Rafael <prats@pyganflor.com>
2019-07-11 10:06:35 -0500

    Update Rafael

--- resources/views/adminlte/crm/fenograma_ejecucion/partials/filtrar_ciclos.blade.php
+++ resources/views/adminlte/crm/fenograma_ejecucion/partials/filtrar_ciclos.blade.php
@@ -1,21 +1,22 @@
 <div style="overflow-x: scroll">
-    <table class="table-striped table-bordered" width="100%" style="border: 2px solid #9d9d9d" id="table_fenograma_ejecucion">
+    <table data-order='[[ 3, "desc" ]]' class="table-striped table-bordered" width="100%" style="border: 2px solid #9d9d9d"
+           id="table_fenograma_ejecucion">
         <thead>
         <tr style="background-color: #357ca5; color: white">
             <th class="text-center" style="border-color: #9d9d9d">
                 Módulo
             </th>
-            <th class="text-center" style="border-color: #9d9d9d">
+            <th class="text-center" style="border-color: #9d9d9d" width="95px">
                 Inicio
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
-                Poda/Siembra
-            </th>
-            <th class="text-center" style="border-color: #9d9d9d">
-                Área m<sup>2</sup>
+                P/S
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
                 Días
+            </th>
+            <th class="text-center" style="border-color: #9d9d9d">
+                Área m<sup>2</sup>
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
                 1ra Flor
@@ -29,9 +30,6 @@
             <th class="text-center" style="border-color: #9d9d9d">
                 Tallos/m<sup>2</sup>
             </th>
-            <th class="text-center" style="border-color: #9d9d9d">
-                Final
-            </th>
         </tr>
         </thead>
         <tbody>
@@ -40,6 +38,7 @@
             $ciclo = 0;
             $total_tallos = 0;
             $total_tallos_m2 = 0;
+            $positivos_tallos_m2 = 0;
         @endphp
         @foreach($ciclos as $item)
             <tr>
@@ -53,14 +52,14 @@
                     {{$item->modulo->getPodaSiembraByCiclo($item->id_ciclo)}}
                 </td>
                 <td class="text-center" style="border-color: #9d9d9d">
-                    {{number_format($item->area, 2)}}
-                </td>
-                <td class="text-center" style="border-color: #9d9d9d">
                     @if($item->fecha_fin != '')
                         {{difFechas($item->fecha_fin, $item->fecha_inicio)->days}}
                     @else
                         {{difFechas(date('Y-m-d'), $item->fecha_inicio)->days}}
                     @endif
+                </td>
+                <td class="text-center" style="border-color: #9d9d9d">
+                    {{number_format($item->area, 2)}}
                 </td>
                 <td class="text-center" style="border-color: #9d9d9d">
                     @if($item->fecha_cosecha != '')
@@ -71,13 +70,10 @@
                     {{$item->get80Porciento()}}
                 </td>
                 <td class="text-center" style="border-color: #9d9d9d">
-                    {{$item->getTallosCosechados()}}
+                    {{number_format($item->getTallosCosechados())}}
                 </td>
                 <td class="text-center" style="border-color: #9d9d9d">
                     {{round($item->getTallosCosechados()/$item->area, 2)}}
-                </td>
-                <td class="text-center" style="border-color: #9d9d9d">
-                    {{$item->fecha_fin}}
                 </td>
             </tr>
             @php
@@ -85,6 +81,9 @@
                 $ciclo += $item->fecha_fin != '' ? difFechas($item->fecha_fin, $item->fecha_inicio)->days : difFechas(date('Y-m-d'), $item->fecha_inicio)->days;
                 $total_tallos += $item->getTallosCosechados();
                 $total_tallos_m2 += round($item->getTallosCosechados()/$item->area, 2);
+                if($item->getTallosCosechados() > 0){
+                    $positivos_tallos_m2 ++;
+                }
             @endphp
         @endforeach
         </tbody>
@@ -93,18 +92,22 @@
                 Totales
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
-                {{number_format($total_area, 2)}}
+                {{count($ciclos) > 0 ? round($ciclo / count($ciclos), 2) : 0}}
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
-                {{count($ciclos) > 0 ? round($ciclo / count($ciclos), 2) : 0}}
+                {{number_format(round($total_area/ 10000, 2), 2)}}
             </th>
             <th class="text-center" colspan="2" style="border-color: #9d9d9d">
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
-                {{number_format($total_area, 2)}}
+                {{number_format($total_tallos, 2)}}
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
-                {{count($ciclos) > 0 ? round($total_tallos_m2 / count($ciclos), 2) : 0}}
+                @if($positivos_tallos_m2 > 0)
+                    {{count($ciclos) > 0 ? round($total_tallos_m2 / $positivos_tallos_m2, 2) : 0}}
+                @else
+                    0
+                @endif
             </th>
             <th class="text-center" style="border-color: #9d9d9d">
             </th>
