d256c81b62193770a53c91bf2413992e0f005581
Rafael <prats@pyganflor.com>
2019-07-18 12:06:00 -0500

    Update Rafael

--- resources/views/adminlte/gestion/sectores_modulos/forms/add_modulo.blade.php
+++ resources/views/adminlte/gestion/sectores_modulos/forms/add_modulo.blade.php
@@ -19,13 +19,13 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-6">
             <div class="form-group">
                 <label for="nombre">Área <em>(m<sup>2</sup>)</em></label>
                 <input type="number" id="area" name="area" class="form-control text-center" maxlength="11" min="1">
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-6">
             <div class="form-group">
                 <label for="nombre">Descripción</label>
                 <textarea name="descripcion" id="descripcion" rows="3" class="form-control text-justify contador" style="width: 100%"
