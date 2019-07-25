d05f7e0ec045937e4f8a5eb037bdfa79d7e5de5a
Rafael <prats@pyganflor.com>
2019-07-25 12:56:44 -0500

    Update Rafael

---
+++ routes/proyecciones/cosecha.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('proy_cosecha', 'Proyecciones\proyCosechaController@inicio');
+Route::get('proy_cosecha/listar_proyecciones', 'Proyecciones\proyCosechaController@listar_proyecciones');
