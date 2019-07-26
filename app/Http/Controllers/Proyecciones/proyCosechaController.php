0c8a019c296b7c3d2fcab0b60f6f6524065b9bef
Rafael <prats@pyganflor.com>
2019-07-26 08:14:26 -0500

    Update Rafael

--- app/Http/Controllers/Proyecciones/proyCosechaController.php
+++ app/Http/Controllers/Proyecciones/proyCosechaController.php
@@ -4,6 +4,7 @@ namespace yura\Http\Controllers\Proyecciones;
 
 use Illuminate\Http\Request;
 use yura\Http\Controllers\Controller;
+use yura\Modelos\Semana;
 use yura\Modelos\Submenu;
 
 class proyCosechaController extends Controller
@@ -18,6 +19,16 @@ class proyCosechaController extends Controller
 
     public function listar_proyecciones(Request $request)
     {
-        dd($request->all());
+        //dd($request->all());
+
+        $array_semanas = [];
+        for ($i = $request->desde; $i <= $request->hasta; $i++) {
+            $semana = Semana::All()->where('estado', 1)->where('codigo', $i)->first();
+            if ($semana != '')
+                if (!in_array($semana->codigo, $array_semanas))
+                    array_push($array_semanas, $semana->codigo);
+        }
+
+        dd($array_semanas);
     }
 }
