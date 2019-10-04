08f3832345bc25fff43ffbdb8486dbf85110fef9
Obrian Villasmil <obrian@pyganflor.com>
2019-10-04 12:59:48 -0500

    updates

--- app/Modelos/ProyeccionVentaSemanalReal.php
+++ app/Modelos/ProyeccionVentaSemanalReal.php
@@ -4,10 +4,10 @@ namespace yura\Modelos;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ProyeccionVentaSemanal extends Model
+class ProyeccionVentaSemanalReal extends Model
 {
-    protected $table = 'proyeccion_venta';
-    protected $primaryKey = 'id_proyeccion_venta_semanal';
+    protected $table = 'proyeccion_venta_semanal_real';
+    protected $primaryKey = 'id_proyeccion_venta_semanal_real';
     public $incrementing = false;
     public $timestamps = false;
 
@@ -20,6 +20,9 @@ class ProyeccionVentaSemanal extends Model
         'cajas_equivalentes',
         'codigo_semana',
         'cajas_fisicas',
+        'valor_proy',
+        'cajas_equivalentes_proy',
+        'cajas_fisicas_proy'
     ];
 
 }
