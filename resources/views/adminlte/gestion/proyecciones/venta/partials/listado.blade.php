3fe00289d7446389cf9ff015efaba43032ca5401
Obrian Villasmil <obrian@pyganflor.com>
2019-11-11 22:01:56 +0000

    updates

--- resources/views/adminlte/gestion/proyecciones/venta/partials/listado.blade.php
+++ resources/views/adminlte/gestion/proyecciones/venta/partials/listado.blade.php
@@ -194,13 +194,8 @@
                     </tr>
                     <tr style="background-color: #e9ecef">
                         <td class="text-center" style="border:2px solid #000000;width: 250px"><b>Saldo final</b></td>
-                        @php $x=0; @endphp
                         @foreach($semanas as $semana => $item)
-                            @php
-                                $saldoInicial= getObjSemana($semana-1)->getSaldoInicial($idVariedad);
-                                dump($saldoInicial);
-                                $saldoFinal = getObjSemana($semana)->getSaldoFinal($idVariedad);
-                            @endphp
+                            @php $saldoFinal = getObjSemana($semana)->getSaldoFinal($idVariedad); @endphp
                             <td style="border: 1px solid #9d9d9d;border: 2px solid #000000;" colspan="3">
                                 <div style="width:100%;text-align:center;" data-toggle="tooltip" data-placement="top" title="Cajas físicas proyectadas">
                                     <b class="{{$saldoFinal < 0 ? "text-red" : "text-success"}}">
