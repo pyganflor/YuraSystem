995f9c4569ffaab251f7b4a2f7d459ef1a3f06ff
Obrian Villasmil <obrian@pyganflor.com>
2019-11-20 16:37:21 -0500

    updates

--- app/Http/Controllers/CRM/CrmProyeccionesController.php
+++ app/Http/Controllers/CRM/CrmProyeccionesController.php
@@ -14,12 +14,12 @@ class CrmProyeccionesController extends Controller
         return view('adminlte.crm.proyecciones.inicio',[
             'url' => $request->getRequestUri(),
             'submenu' => Submenu::Where('url', '=', substr($request->getRequestUri(), 1))->get()[0],
-            'text' => ['titulo'=>'Dashboard','subtitulo'=>'Proyección de ventas'],
+            'text' => ['titulo'=>'Dashboard','subtitulo'=>'Proyecciones'],
             'indicador' => Indicador::whereIn('nombre',['DP1','DP2','DP3','DP4','DP5','DP6','DP7','DP8','DP9'])->select('valor')->get()
         ]);
     }
 
     public function desgloseIndicador(Request $request){
-        return view();
+        return view('adminlte.crm.proyecciones.partials.modal_cosechado');
     }
 }
