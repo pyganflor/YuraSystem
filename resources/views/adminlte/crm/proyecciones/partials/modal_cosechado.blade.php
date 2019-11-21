faf3e635b19407bcf4b84466973956ee2a4b4f40
Obrian Villasmil <obrian@pyganflor.com>
2019-11-21 21:45:46 +0000

    updates

--- resources/views/adminlte/crm/proyecciones/partials/modal_cosechado.blade.php
+++ resources/views/adminlte/crm/proyecciones/partials/modal_cosechado.blade.php
@@ -12,26 +12,54 @@
             <canvas id="chart2" style="margin-top: 5px"></canvas>
         </div>
         <div class="tab-pane" id="tab_3">
-            <table class="table-striped table-responsive table-bordered" width="100%" style="border: 2px solid #9d9d9d">
-                <thead>
-                    <tr>01</tr>
-                    <tr>012</tr>
-                    <tr>03</tr>
-                    <tr>04</tr>
-                </thead>
+            <table class="table table-bordered">
                 <tbody>
-
-                </tbody>
-            </table>
-            <table class="table-striped table-responsive table-bordered " width="100%" style="border: 2px solid #9d9d9d;margin-top:15px">
-                <thead>
-                <tr>01</tr>
-                <tr>012</tr>
-                <tr>03</tr>
-                <tr>04</tr>
-                </thead>
-                <tbody>
-
+                <tr>
+                    <th style="width: 10px">Variedad/Semana</th>
+                    <th>Task</th>
+                    <th>Progress</th>
+                    <th style="width: 40px">Label</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Update software</td>
+                    <td>
+                        <div class="progress progress-xs">
+                            <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                        </div>
+                    </td>
+                    <td><span class="badge bg-red">55%</span></td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Clean database</td>
+                    <td>
+                        <div class="progress progress-xs">
+                            <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
+                        </div>
+                    </td>
+                    <td><span class="badge bg-yellow">70%</span></td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Cron job running</td>
+                    <td>
+                        <div class="progress progress-xs progress-striped active">
+                            <div class="progress-bar progress-bar-primary" style="width: 30%"></div>
+                        </div>
+                    </td>
+                    <td><span class="badge bg-light-blue">30%</span></td>
+                </tr>
+                <tr>
+                    <td>4.</td>
+                    <td>Fix and squish bugs</td>
+                    <td>
+                        <div class="progress progress-xs progress-striped active">
+                            <div class="progress-bar progress-bar-success" style="width: 90%"></div>
+                        </div>
+                    </td>
+                    <td><span class="badge bg-green">90%</span></td>
+                </tr>
                 </tbody>
             </table>
         </div>
