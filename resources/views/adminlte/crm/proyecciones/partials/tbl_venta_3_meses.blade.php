0c5f5d596e94f062b9b0f26f437c9b410818c31b
Obrian Villasmil <obrian@pyganflor.com>
2019-11-25 13:57:28 -0500

    updates

--- resources/views/adminlte/crm/proyecciones/partials/tbl_venta_3_meses.blade.php
+++ resources/views/adminlte/crm/proyecciones/partials/tbl_venta_3_meses.blade.php
@@ -3,7 +3,7 @@
         <tr>
             <td class="bg-gray-light text-center" style="border:1px solid">Mes</td>
             @foreach($data as $mes=> $valor)
-                <td class="bg-gray-light text-center" style="border-bottom:1px solid" >
+                <td class="bg-gray-light text-center" style="border:1px solid" >
                     <span data-toggle="tooltip" title="Mes" style="border:1px solid">{{$mes}}</span></span>
                 </td>
             @endforeach
@@ -12,7 +12,7 @@
         <tr>
             <td class="bg-gray-light text-center" style="border:1px solid">Valor</td>
             @foreach($data as $mes=> $valor)
-                <td class="text-center" style="border-bottom:1px solid" >
+                <td class="text-center" style="border:1px solid" >
                     <span data-toggle="tooltip" title="Cajas">{{$valor}}</span></span>
                 </td>
             @endforeach
