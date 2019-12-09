15c55ec500aeae153b2c6b2a4fe48481bf0675e7
Obrian Villasmil <obrian@pyganflor.com>
2019-12-09 10:50:47 -0500

    updayes

--- resources/views/adminlte/gestion/db/intervalos_indicadores/script.blade.php
+++ resources/views/adminlte/gestion/db/intervalos_indicadores/script.blade.php
@@ -2,7 +2,8 @@
     listar_indicadores();
 
     function listar_indicadores(){
-        $.get('{{url('intervalo_indicador/listar')}}', datos, function (retorno) {
+        $.get('{{url('intervalo_indicador/listar')}}', {}, function (retorno) {
+            console.log(retorno);
             $('#div_listado_intervalo_indicador').html(retorno);
         }).always(function () {
             $.LoadingOverlay('hide');
