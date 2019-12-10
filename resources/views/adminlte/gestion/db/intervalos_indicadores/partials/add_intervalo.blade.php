3ea72b6589380003e2690b6ab6284bb829b04e0e
Obrian Villasmil <obrian@pyganflor.com>
2019-12-10 15:48:07 -0500

    updates

--- resources/views/adminlte/gestion/db/intervalos_indicadores/partials/add_intervalo.blade.php
+++ resources/views/adminlte/gestion/db/intervalos_indicadores/partials/add_intervalo.blade.php
@@ -1,10 +1,11 @@
+@dump(getIntervalosIndicador('D1')))
 <div class="text-right" style="margin-bottom: 10px">
     <button class="btn btn-primary btn-xs" title="Agregar intervalo de rango" onclick="add_row('rango')">
         <i class="fa fa-plus"></i>
     </button>
-    <button class="btn btn-success btn-xs" title="Agregar intervalo de condición" onclick="add_row('condicion')">
+    {{--<button class="btn btn-success btn-xs" title="Agregar intervalo de condición" onclick="add_row('condicion')">
         <i class="fa fa-plus"></i>
-    </button>
+    </button>--}}
 </div>
 <form id="form_add_intervalo" class="form_rows_intervalos">
     <input type="hidden" id="id_indicador" value="{{$indicador}}">
