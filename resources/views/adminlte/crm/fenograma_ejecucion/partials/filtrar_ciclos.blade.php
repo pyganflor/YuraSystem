b67e91e6e96dd11a3cd49a5573b35b43883350a1
Rafael <prats@pyganflor.com>
2019-12-11 10:28:32 -0500

    updates

--- resources/views/adminlte/crm/fenograma_ejecucion/partials/filtrar_ciclos.blade.php
+++ resources/views/adminlte/crm/fenograma_ejecucion/partials/filtrar_ciclos.blade.php
@@ -136,10 +136,10 @@
 
                             if($pos_item + 1 < count($ciclos)){
                                 if($ciclos[$pos_item + 1]->semana()->codigo != $codigo_semana){
-                                    echo number_format($area / 10000, 2);
+                                    echo $area;
                                 }
                             } else {
-                                echo number_format($area / 10000, 2);
+                                echo $area;
                             }
                         @endphp
                     </td>
