d6277cfdfddf147df475e8b5f23334891642e684
Rafael <prats@pyganflor.com>
2019-12-18 11:42:20 -0500

    updates

--- app/Http/Controllers/Costos/CostosController.php
+++ app/Http/Controllers/Costos/CostosController.php
@@ -651,7 +651,7 @@ class CostosController extends Controller
             $activeSheetData = $document->getActiveSheet()->toArray(null, true, true, true);
 
             $titles = $activeSheetData[1];
-            dd($activeSheetData);
+            //dd($activeSheetData);
             foreach ($activeSheetData as $pos_row => $row) {
                 if ($pos_row > 1) {
                     if ($row['A'] != '' && $row['B'] != '') {
@@ -680,6 +680,7 @@ class CostosController extends Controller
                             foreach ($titles as $pos_title => $t) {
                                 if (!in_array($pos_title, ['A', 'B'])) {
                                     $codigo_semana = intval($t);
+                                    $value = floatval(str_replace(',', '', $row[$pos_title]));
                                     $costos = CostosSemana::All()
                                         ->where('codigo_semana', $codigo_semana)
                                         ->where('id_actividad_producto', $act_prod->id_actividad_producto)
@@ -690,17 +691,26 @@ class CostosController extends Controller
                                         $model->codigo_semana = $codigo_semana;
                                         $model->fecha_registro = date('Y-m-d H:i:s');
                                         if ($request->criterio_importar == 'V')  // dinero
-                                            $model->valor = '';
+                                            $model->valor = $value;
                                         else    //
-                                            $model->cantidad = '';
+                                            $model->cantidad = $value;
 
                                         $model->save();
                                         $costos = CostosSemana::All()->last();
-                                        bitacora('costos_semana', $act_prod->id_costos_semana, 'I', 'Inserción satisfactoria de un nuevo costos_semana');
-                                    }
+                                        bitacora('costos_semana', $costos->id_costos_semana, 'I', 'Inserción satisfactoria de un nuevo costos_semana');
+                                    } else {
+                                        if ($request->criterio_importar == 'V')  // dinero
+                                            $costos->valor = $value;
+                                        else    //
+                                            $costos->cantidad = $value;
 
+                                        $costos->save();
+                                        bitacora('costos_semana', $costos->id_costos_semana, 'I', 'Inserción satisfactoria de un nuevo costos_semana');
+                                    }
                                 }
                             }
+
+                            $msg .= '<li class="bg-green">Se ha importado el producto: "' . $producto->nombre . '" en la actividad: "' . $actividad->nombre . '</li>';
                         }
                     }
                 }
