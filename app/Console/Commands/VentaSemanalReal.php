a15513fafc9773d6eac0696d1335908b9998d487
Obrian Villasmil <obrian@pyganflor.com>
2020-01-10 15:38:22 -0500

    updates

--- app/Console/Commands/VentaSemanalReal.php
+++ app/Console/Commands/VentaSemanalReal.php
@@ -138,7 +138,7 @@ class VentaSemanalReal extends Command
                                 $objProySemReal->valor =0;
                                 $objProySemReal->cajas_equivalentes = 0;
                                 $objProySemReal->cajas_fisicas = 0;
-                                $objProySemReal->cajas_fisicas_anno_anterior = isset($objProyeccionVentaSemanalAnoAnterior->cajas_fisicas) ? $objProyeccionVentaSemanalAnoAnterior->cajas_fisicas : 0;
+                               // $objProySemReal->cajas_fisicas_anno_anterior = isset($objProyeccionVentaSemanalAnoAnterior->cajas_fisicas) ? $objProyeccionVentaSemanalAnoAnterior->cajas_fisicas : 0;
                                 foreach ($pedidos as $pedido){
                                     //if($variedad->id_variedad==2 )
                                        // dd($pedido->getCajasFullByVariedad($variedad->id_variedad));
