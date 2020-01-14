94305ba0ab718640b1a1f0ba35c7c1554bc8d247
Rafael <prats@pyganflor.com>
2020-01-14 14:11:13 -0500

    updates

--- app/Http/Controllers/Indicadores/Costos.php
+++ app/Http/Controllers/Indicadores/Costos.php
@@ -69,16 +69,25 @@ class Costos
             $sem_desde = getSemanaByDate(opDiasFecha('-', 35, date('Y-m-d')));
             $sem_hasta = getSemanaByDate(opDiasFecha('-', 7, date('Y-m-d')));
 
-            $insumos = DB::table('costos_semana')
-                ->select(DB::raw('sum(valor) as cant'))
-                ->where('codigo_semana', '>=', $sem_desde->codigo)
-                ->where('codigo_semana', '<=', $sem_hasta->codigo)
+            $insumos = DB::table('costos_semana as c')
+                ->select(DB::raw('sum(c.valor) as cant'))
+                ->join('actividad_producto as ac', 'ac.id_actividad_producto', '=', 'c.id_actividad_producto')
+                ->join('actividad as a', 'a.id_actividad', '=', 'ac.id_actividad')
+                ->join('area as ar', 'ar.id_area', '=', 'a.id_area')
+                ->where('ar.nombre', '=', 'CAMPO')
+                ->where('c.codigo_semana', '>=', $sem_desde->codigo)
+                ->where('c.codigo_semana', '<=', $sem_hasta->codigo)
                 ->get()[0]->cant;
-            $mano_obra = DB::table('costos_semana_mano_obra')
-                ->select(DB::raw('sum(valor) as cant'))
-                ->where('codigo_semana', '>=', $sem_desde->codigo)
-                ->where('codigo_semana', '<=', $sem_hasta->codigo)
+            $mano_obra = DB::table('costos_semana_mano_obra as c')
+                ->select(DB::raw('sum(c.valor) as cant'))
+                ->join('actividad_mano_obra as am', 'am.id_actividad_mano_obra', '=', 'c.id_actividad_mano_obra')
+                ->join('actividad as a', 'a.id_actividad', '=', 'am.id_actividad')
+                ->join('area as ar', 'ar.id_area', '=', 'a.id_area')
+                ->where('ar.nombre', '=', 'CAMPO')
+                ->where('c.codigo_semana', '>=', $sem_desde->codigo)
+                ->where('c.codigo_semana', '<=', $sem_hasta->codigo)
                 ->get()[0]->cant;
+
             $costos_total = $insumos + $mano_obra;
             $area = getIndicadorByName('D7');   // Área en producción (-4 semanas)
 
