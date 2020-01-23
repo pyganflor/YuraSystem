75488c7ed07055f1a979ddeac3473b3212b3ee12
Rafael <prats@pyganflor.com>
2020-01-23 17:08:09 -0500

    rafa

---
+++ resources/views/adminlte/crm/indicadores_claves_costos.blade.php
@@ -0,0 +1,41 @@
+<div class="box box-info">
+    <div class="box-body">
+        <div style="overflow-x: scroll">
+            <table class="table-responsive" width="100%">
+                <tr>
+                    <th class="text-center">Campo/<sup>ha</sup>/semana</th>
+                    <th class="text-center">Cosecha x Tallo</th>
+                    <th class="text-center">Postcosecha x Tallo</th>
+                    <th class="text-center">Total x Tallo</th>
+                </tr>
+                <tr>
+                    <th class="text-center">
+                        <canvas id="canvas_campo_ha_semana" style="width: 210px"></canvas>
+                    </th>
+                    <th class="text-center">
+                        <canvas id="canvas_cosecha_x_tallo" style="width: 210px"></canvas>
+                    </th>
+                    <th class="text-center">
+                        <canvas id="canvas_postcosecha_x_tallo" style="width: 210px"></canvas>
+                    </th>
+                    <th class="text-center">
+                        <canvas id="canvas_total_x_tallo" style="width: 210px"></canvas>
+                    </th>
+                </tr>
+                <tr>
+                    <th class="text-center">{{number_format(explode('|', $costos_campo_semana)[0], 2)}}</th>
+                    <th class="text-center">{{number_format($costos_cosecha_x_tallo, 4)}}</th>
+                    <th class="text-center">{{number_format($costos_postcosecha_x_tallo, 4)}}</th>
+                    <th class="text-center">{{number_format($costos_total_x_tallo, 4)}}</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+
+<script>
+    render_gauge('canvas_campo_ha_semana', '{{number_format(explode('|', $costos_campo_semana)[0], 2)}}', rangos_costos_campo_ha_semana, true);
+    render_gauge('canvas_cosecha_x_tallo', '{{number_format($costos_cosecha_x_tallo, 4)}}', rangos_costos_cosecha_tallo, true);
+    render_gauge('canvas_postcosecha_x_tallo', '{{number_format($costos_postcosecha_x_tallo, 4)}}', rangos_costos_postcosecha_tallo, true);
+    render_gauge('canvas_total_x_tallo', '{{number_format($costos_total_x_tallo, 4)}}', rangos_costos_total_tallo, true);
+</script>
