775e64646e59767e0f7ed6241a5fd22826b6ba48
Obrian Villasmil <obrian@pyganflor.com>
2020-01-27 11:46:38 -0500

    updates

--- app/Console/Commands/VentaSemanalReal.php
+++ app/Console/Commands/VentaSemanalReal.php
@@ -151,8 +151,11 @@ class VentaSemanalReal extends Command
                                                 $objProySemReal->valor += $pedido->getPrecioByVariedad($variedad->id_variedad);
                                                 $objProySemReal->cajas_equivalentes += $pedido->getCajasByVariedad($variedad->id_variedad);
                                                 $objProySemReal->cajas_fisicas += $pedido->getCajasFullByVariedad($variedad->id_variedad);
-                                                if(isset($objProyeccionVentaSemanalAnoAnterior->cajas_fisicas) && $objProyeccionVentaSemanalAnoAnterior->cajas_fisicas>0)
+                                                if(isset($objProyeccionVentaSemanalAnoAnterior->cajas_fisicas) && $objProyeccionVentaSemanalAnoAnterior->cajas_fisicas>0){
                                                     $objProySemReal->cajas_fisicas_anno_anterior =  $objProyeccionVentaSemanalAnoAnterior->cajas_fisicas;
+                                                    Info("Semana: ".$objProyeccionVentaSemanalAnoAnterior->codigo_semana." cajas: " .$objProyeccionVentaSemanalAnoAnterior->cajas_fisicas);
+                                                }
+
                                             }
                                         /*$proyeccionVentaSemanal = ProyeccionVentaSemanal::where([
                                                 ['id_variedad',$variedad->id_variedad],
