b1770c7e3095aa94935dd3d714fa19f30027e4d1
Rafael <prats@pyganflor.com>
2020-01-28 16:45:03 -0500

    rafa

--- app/Http/Controllers/Indicadores/Costos.php
+++ app/Http/Controllers/Indicadores/Costos.php
@@ -370,4 +370,44 @@ class Costos
             $model->save();
         }
     }
+
+    public function costos_m2_16_semanas_atras()
+    {
+        $model = getIndicadorByName('C9');
+        if ($model != '') {
+            $dias = 7;
+            $last_semana = '';
+            $valor = 0;
+            while ($valor <= 0) {
+                $dias += 7;
+                $last_semana = getSemanaByDate(opDiasFecha('-', $dias, date('Y-m-d')));
+                if ($last_semana != '') {
+                    $valor = DB::table('costos_semana')
+                        ->select(DB::raw('sum(valor) as cant'))
+                        ->where('codigo_semana', $last_semana->codigo)
+                        ->get()[0]->cant;
+                } else {
+                    return false;
+                }
+            }
+
+            $sem_desde = getSemanaByDate(opDiasFecha('-', 105, $last_semana->fecha_inicial));
+            $sem_hasta = $last_semana;
+
+            $costos = DB::table('resumen_costos_semanal')
+                ->select(DB::raw('sum(mano_obra + insumos + fijos + regalias) as cant'))
+                ->where('codigo_semana', '>=', $sem_desde->codigo)
+                ->where('codigo_semana', '<=', $sem_hasta->codigo)
+                ->get()[0]->cant;
+            $area = DB::table('resumen_area_semanal')
+                ->select(DB::raw('sum(area) as cant'))
+                ->where('codigo_semana', '>=', $sem_desde->codigo)
+                ->where('codigo_semana', '<=', $sem_hasta->codigo)
+                ->get()[0]->cant;
+
+            $valor = $area > 0 ? round($costos / $area, 2) : 0;
+            $model->valor = $valor;
+            $model->save();
+        }
+    }
 }
