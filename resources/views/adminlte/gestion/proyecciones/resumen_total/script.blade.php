fb4a44090b836208137cb809da17b16ace081279
Obrian Villasmil <obrian@pyganflor.com>
2020-02-05 15:11:02 +0000

    updates

--- resources/views/adminlte/gestion/proyecciones/resumen_total/script.blade.php
+++ resources/views/adminlte/gestion/proyecciones/resumen_total/script.blade.php
@@ -6,7 +6,7 @@
         data= {
             desde: $(".desde").val(),
             hasta: $(".hasta").val(),
-            id_variredad : $("#filtro_predeterminado_variedad").val()
+            id_variedad : $("#filtro_predeterminado_variedad").val()
         };
         get_jquery('{{url('proy_resumen_total/listar_resumen_total')}}', data, function (retorno) {
             $('#listado_proyecciones_resumen_total').html(retorno);
