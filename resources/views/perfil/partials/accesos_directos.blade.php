1385e8e9509af82d8f66d1adfcd3954f0c197442
Rafael <prats@pyganflor.com>
2020-02-06 07:59:55 -0500

    rafa

--- resources/views/perfil/partials/accesos_directos.blade.php
+++ resources/views/perfil/partials/accesos_directos.blade.php
@@ -18,36 +18,38 @@
                         <span class="badge">0</span>
                     </th>
                 </tr>
-                @foreach($m->submenus->where('estado', 'A')->sortBy('nombre') as $s)
-                    <tr class="tr_submenu_from_{{$m->id_menu}} hide" id="tr_submenu_{{$s->id_submenu}}"
-                        onmouseover="$(this).css('background-color', '#77dbf9')" onmouseleave="$(this).css('background-color', '')">
-                        <td class="text-left" style="border-color: #9d9d9d; width: 40%">
-                            <label for="check_{{$s->id_submenu}}" class="mouse-hand" style="margin-left: 25px">{!! $s->nombre !!}</label>
-                            <span class="pull-right text-green" id="icon_selected_{{$s->id_submenu}}">
+                @foreach($m->submenus_activos as $s)
+                    @if(isActive_action($s->id_submenu))
+                        <tr class="tr_submenu_from_{{$m->id_menu}} hide" id="tr_submenu_{{$s->id_submenu}}"
+                            onmouseover="$(this).css('background-color', '#77dbf9')" onmouseleave="$(this).css('background-color', '')">
+                            <td class="text-left" style="border-color: #9d9d9d; width: 40%">
+                                <label for="check_{{$s->id_submenu}}" class="mouse-hand" style="margin-left: 25px">{!! $s->nombre !!}</label>
+                                <span class="pull-right text-green" id="icon_selected_{{$s->id_submenu}}">
                             </span>
-                        </td>
-                        <td class="text-center" style="border-color: #9d9d9d">
-                            <div style="overflow-x: scroll; max-width: 300px; margin: 0 auto">
-                                <table class="table-bordered" style="width: 100%">
-                                    <tr>
-                                        @foreach($iconos as $i)
-                                            <td class="text-center mouse-hand" style="border-color: #9d9d9d; width: 45px"
-                                                onmouseover="$(this).css('background-color', 'white')"
-                                                onmouseleave="$(this).css('background-color', '')"
-                                                onclick="select_icono('{{$i->id_icono}}', '{{$i->nombre}}', '{{$s->id_submenu}}')">
-                                                <i class="fa fa-fw fa-{{$i->nombre}}" style="margin-right: 5px; margin-left: 5px"></i>
-                                            </td>
-                                        @endforeach
-                                    </tr>
-                                </table>
-                            </div>
-                            <input type="hidden" id="id_icon_{{$s->id_submenu}}">
-                        </td>
-                        <th class="text-center mouse-hand" style="border-color: #9d9d9d; width: 20px">
-                            <input type="checkbox" id="check_{{$s->id_submenu}}" onchange="seleccionar_submenu('{{$s->id_submenu}}')"
-                                    {{in_array($s->id_submenu, $ids_submenu_ad) ? 'checked' : ''}}>
-                        </th>
-                    </tr>
+                            </td>
+                            <td class="text-center" style="border-color: #9d9d9d">
+                                <div style="overflow-x: scroll; max-width: 300px; margin: 0 auto">
+                                    <table class="table-bordered" style="width: 100%">
+                                        <tr>
+                                            @foreach($iconos as $i)
+                                                <td class="text-center mouse-hand" style="border-color: #9d9d9d; width: 45px"
+                                                    onmouseover="$(this).css('background-color', 'white')"
+                                                    onmouseleave="$(this).css('background-color', '')"
+                                                    onclick="select_icono('{{$i->id_icono}}', '{{$i->nombre}}', '{{$s->id_submenu}}')">
+                                                    <i class="fa fa-fw fa-{{$i->nombre}}" style="margin-right: 5px; margin-left: 5px"></i>
+                                                </td>
+                                            @endforeach
+                                        </tr>
+                                    </table>
+                                </div>
+                                <input type="hidden" id="id_icon_{{$s->id_submenu}}">
+                            </td>
+                            <th class="text-center mouse-hand" style="border-color: #9d9d9d; width: 20px">
+                                <input type="checkbox" id="check_{{$s->id_submenu}}" onchange="seleccionar_submenu('{{$s->id_submenu}}')"
+                                        {{in_array($s->id_submenu, $ids_submenu_ad) ? 'checked' : ''}}>
+                            </th>
+                        </tr>
+                    @endif
                 @endforeach
             @endforeach
         @endforeach
