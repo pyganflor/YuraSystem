2b3f2d291549afcef2aee2f56377877787d5a4e8
Rafael <prats@pyganflor.com>
2020-02-07 12:38:41 -0500

    rafa

--- app/Console/Commands/UpdateHistoricoVentas.php
+++ app/Console/Commands/UpdateHistoricoVentas.php
@@ -3,10 +3,13 @@
 namespace yura\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use yura\Modelos\Cliente;
 use yura\Modelos\HistoricoVentas;
 use yura\Modelos\Pedido;
 use yura\Modelos\Sector;
+use yura\Modelos\Semana;
 
 class UpdateHistoricoVentas extends Command
 {
@@ -15,7 +18,7 @@ class UpdateHistoricoVentas extends Command
      *
      * @var string
      */
-    protected $signature = 'historico_ventas:update';
+    protected $signature = 'historico_ventas:update {desde=0} {hasta=0}';
 
     /**
      * The console command description.
@@ -41,51 +44,95 @@ class UpdateHistoricoVentas extends Command
      */
     public function handle()
     {
-        $pedidos = Pedido::All()->where('estado', 1)
-            ->where('historico', 0)
-            ->where('fecha_pedido', '<', date('Y-m-d'));
+        $ini = date('Y-m-d H:i:s');
+        Log::info('<<<<< ! >>>>> Ejecutando comando "historico_ventas:update" <<<<< ! >>>>>');
 
-        if (count($pedidos) > 0) {
-            Log::info('<<<<< ! >>>>> Ejecutando comando "historico_ventas:update" <<<<< ! >>>>>');
-            foreach ($pedidos as $p) {
-                if (!getFacturaAnulada($p->id_pedido)) {
-                    foreach ($p->getVariedades() as $v) {
-                        $historico = HistoricoVentas::All()
-                            ->where('id_cliente', $p->id_cliente)
-                            ->where('id_variedad', $v)
-                            ->where('mes', substr($p->fecha_pedido, 5, 2))
-                            ->where('anno', substr($p->fecha_pedido, 0, 4))
+        $desde_par = $this->argument('desde');
+        $hasta_par = $this->argument('hasta');
+
+        if ($desde_par <= $hasta_par) {
+            if ($desde_par != 0)
+                $semana_desde = Semana::All()->where('estado', 1)->where('codigo', $desde_par)->first();
+            else
+                $semana_desde = getSemanaByDate(date('Y-m-d'));
+            if ($hasta_par != 0)
+                $semana_hasta = Semana::All()->where('estado', 1)->where('codigo', $hasta_par)->first();
+            else
+                $semana_hasta = getSemanaByDate(date('Y-m-d'));
+
+            $array_semanas = [];
+            for ($i = $semana_desde->codigo; $i <= $semana_hasta->codigo; $i++) {
+                $semana = Semana::All()
+                    ->where('estado', 1)
+                    ->where('codigo', $i)->first();
+                if ($semana != '')
+                    if (!in_array($semana, $array_semanas)) {
+                        array_push($array_semanas, $semana);
+                    }
+            }
+
+            $array_meses = [];
+            foreach ($array_semanas as $sem) {
+                $mes = [
+                    'mes' => substr($sem->fecha_inicial, 5, 2),
+                    'anno' => substr($sem->fecha_inicial, 0, 4)
+                ];
+
+                if (!in_array($mes, $array_meses))
+                    array_push($array_meses, $mes);
+            }
+
+            foreach ($array_meses as $mes) {
+                foreach (Cliente::All()->where('estado', 1) as $cli) {
+                    foreach (getVariedades() as $var) {
+                        $model = HistoricoVentas::All()
+                            ->where('id_cliente', $cli->id_cliente)
+                            ->where('id_variedad', $var->id_variedad)
+                            ->where('mes', $mes['mes'])
+                            ->where('anno', $mes['anno'])
                             ->first();
 
-                        if ($historico != '') {
-                            $historico->valor += $p->getPrecioByPedidoVariedad($v);
-                            $historico->cajas_fisicas += $p->getCajasFisicasByVariedad($v);
-                            $historico->cajas_equivalentes += $p->getCajasByVariedad($v);
-                            $historico->precio_x_ramo = round($historico->valor / ($historico->cajas_fisicas * getConfiguracionEmpresa()->ramos_x_caja), 2);
-                        } else {
-                            $historico = new HistoricoVentas();
-                            $historico->id_cliente = $p->id_cliente;
-                            $historico->id_variedad = $v;
-                            $historico->anno = substr($p->fecha_pedido, 0, 4);
-                            $historico->mes = substr($p->fecha_pedido, 5, 2);
-
-                            $historico->valor = $p->getPrecioByPedidoVariedad($v);
-                            $historico->cajas_fisicas = $p->getCajasFisicasByVariedad($v);
-                            $historico->cajas_equivalentes = $p->getCajasByVariedad($v);
-                            $historico->precio_x_ramo = round($historico->valor / ($historico->cajas_fisicas * getConfiguracionEmpresa()->ramos_x_caja), 2);
+                        if ($model == '') { // es nuevo
+                            $model = new HistoricoVentas();
+                            $model->id_cliente = $cli->id_cliente;
+                            $model->id_variedad = $var->id_variedad;
+                            $model->mes = $mes['mes'];
+                            $model->anno = $mes['anno'];
                         }
 
-                        if (!$historico->save()) {
-                            Log::info('ERROR: Ocurrió un problema con el pedido #' . $p->id_pedido . ' - variedad ' . $v->siglas);
+                        $pedidos = DB::table('pedido')
+                            ->where(DB::raw('month(fecha_pedido) = ' . $mes['mes']))
+                            ->where(DB::raw('year(fecha_pedido) = ' . $mes['anno']))
+                            ->get();
+
+                        $valor = 0;
+                        $cajas_fisicas = 0;
+                        $cajas_equivalentes = 0;
+                        foreach ($pedidos as $p) {
+                            $p = Pedido::find($p->id_pedido);
+                            if (!getFacturaAnulada($p->id_pedido)) {
+                                $valor += $p->getPrecioByPedidoVariedad($var->id_variedad);
+                                $cajas_fisicas += $p->getCajasFullByVariedad($var->id_variedad);
+                                $cajas_equivalentes += $p->getCajasByVariedad($var->id_variedad);
+                            }
+                        }
+                        $precio_x_ramo = $cajas_equivalentes > 0 ? round($valor / ($cajas_equivalentes * getConfiguracionEmpresa()->ramos_x_caja), 2) : 0;
+
+                        $model->valor = $valor;
+                        $model->cajas_fisicas = $cajas_fisicas;
+                        $model->cajas_equivalentes = $cajas_equivalentes;
+                        $model->precio_x_ramo = $precio_x_ramo;
+
+                        if (!$model->save()) {
+                            Log::info('ERROR: Ocurrió un problema con el pedido #' . $p->id_pedido . ' - variedad ' . $var->siglas);
                             return false;
                         }
                     }
-                    $p->historico = 1;
-                    $p->save();
-                    Log::info('Pedido #' . $p->id_pedido . ' procesado');
                 }
             }
-            Log::info('<<<<< * >>>>> Fin satisfactorio del comando "historico_ventas:update" <<<<< * >>>>>');
         }
+        $time_duration = difFechas(date('Y-m-d H:i:s'), $ini)->h . ':' . difFechas(date('Y-m-d H:i:s'), $ini)->m . ':' . difFechas(date('Y-m-d H:i:s'), $ini)->s;
+        Log::info('<*> DURACION: ' . $time_duration . '  <*>');
+        Log::info('<<<<< * >>>>> Fin satisfactorio del comando "historico_ventas:update" <<<<< * >>>>>');
     }
 }
