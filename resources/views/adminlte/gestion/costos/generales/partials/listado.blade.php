10ae62bbef018b84e939db5af5c4bbc8a6523bbd
Rafael <prats@pyganflor.com>
2020-02-18 11:45:55 -0500

    rafa

--- resources/views/adminlte/gestion/costos/generales/partials/listado.blade.php
+++ resources/views/adminlte/gestion/costos/generales/partials/listado.blade.php
@@ -46,11 +46,11 @@
             </span>
         </td>
         @foreach($semanas as $item)
-            <th class="text-center" style="border-color: #9d9d9d">
+            <td class="text-center" style="border-color: #9d9d9d">
                 <div style="width: 100px">
                     ${{number_format($item->campo_mp, 2)}}
                 </div>
-            </th>
+            </td>
         @endforeach
     </tr>
     <tr class="tr_campo hide">
@@ -60,11 +60,205 @@
             </span>
         </td>
         @foreach($semanas as $item)
-            <th class="text-center" style="border-color: #9d9d9d">
+            <td class="text-center" style="border-color: #9d9d9d">
                 <div style="width: 100px">
                     ${{number_format($item->campo_mo, 2)}}
                 </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_campo hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GIP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo_gip, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_campo hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GA
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo_ga, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    {{-- PROPAGACIÓN --}}
+    <tr>
+        <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black; font-weight: bold" class="btn btn-xs btn-link"
+                  onclick="$('.tr_propagacion').toggleClass('hide')">
+                PROPAGACIÓN
+            </span>
+        </th>
+        @php
+            $total_propagacion = 0;
+        @endphp
+        @foreach($semanas as $item)
+            <th class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->propagacion, 2)}}
+                </div>
             </th>
+            @php
+                $total_propagacion += $item->propagacion;
+            @endphp
+        @endforeach
+        <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <div style="width: 110px">
+                ${{number_format($total_propagacion, 2)}}
+            </div>
+        </th>
+    </tr>
+    <tr class="tr_propagacion hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                MP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->propagacion_mp, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_propagacion hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                MO
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->propagacion_mo, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_propagacion hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GIP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->propagacion_gip, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_propagacion hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GA
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->propagacion_ga, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    {{-- COSECHA --}}
+    <tr>
+        <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black; font-weight: bold" class="btn btn-xs btn-link"
+                  onclick="$('.tr_cosecha').toggleClass('hide')">
+                COSECHA
+            </span>
+        </th>
+        @php
+            $total_cosecha = 0;
+        @endphp
+        @foreach($semanas as $item)
+            <th class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->cosecha, 2)}}
+                </div>
+            </th>
+            @php
+                $total_cosecha += $item->cosecha;
+            @endphp
+        @endforeach
+        <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <div style="width: 110px">
+                ${{number_format($total_cosecha, 2)}}
+            </div>
+        </th>
+    </tr>
+    <tr class="tr_cosecha hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                MP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->cosecha_mp, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_cosecha hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                MO
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->cosecha_mo, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_cosecha hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GIP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->cosecha_gip, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_cosecha hide">
+        <td class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GA
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->cosecha_ga, 2)}}
+                </div>
+            </td>
         @endforeach
     </tr>
 </table>
