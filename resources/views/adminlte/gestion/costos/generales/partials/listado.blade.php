19fc9c28bfa9f8c391f8f7b4cf03f59e6df0741d
Rafael <prats@pyganflor.com>
2020-02-20 08:41:37 -0500

    rafa

--- resources/views/adminlte/gestion/costos/generales/partials/listado.blade.php
+++ resources/views/adminlte/gestion/costos/generales/partials/listado.blade.php
@@ -12,88 +12,31 @@
             Total
         </th>
     </tr>
-    {{-- CAMPO --}}
+    {{-- VENTA --}}
     <tr>
         <th class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
-            <span style="margin: auto 5px; color: black; font-weight: bold" class="btn btn-xs btn-link"
-                  onclick="$('.tr_campo').toggleClass('hide')">
-                CAMPO
+            <span style="margin: auto 5px; color: black; font-weight: bold; font-size: 0.85em">
+                VENTA
             </span>
         </th>
         @php
-            $total_campo = 0;
+            $total_valor = 0;
         @endphp
         @foreach($semanas as $item)
             <th class="text-center" style="border-color: #9d9d9d">
                 <div style="width: 100px">
-                    ${{number_format($item->campo, 2)}}
+                    ${{number_format($item->valor, 2)}}
                 </div>
             </th>
             @php
-                $total_campo += $item->campo;
+                $total_valor += $item->valor;
             @endphp
         @endforeach
         <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
             <div style="width: 110px">
-                ${{number_format($total_campo, 2)}}
+                ${{number_format($total_valor, 2)}}
             </div>
         </th>
-    </tr>
-    <tr class="tr_campo hide">
-        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
-            <span style="margin: auto 5px; color: black">
-                MP
-            </span>
-        </td>
-        @foreach($semanas as $item)
-            <td class="text-center" style="border-color: #9d9d9d">
-                <div style="width: 100px">
-                    ${{number_format($item->campo_mp, 2)}}
-                </div>
-            </td>
-        @endforeach
-    </tr>
-    <tr class="tr_campo hide">
-        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
-            <span style="margin: auto 5px; color: black">
-                MO
-            </span>
-        </td>
-        @foreach($semanas as $item)
-            <td class="text-center" style="border-color: #9d9d9d">
-                <div style="width: 100px">
-                    ${{number_format($item->campo_mo, 2)}}
-                </div>
-            </td>
-        @endforeach
-    </tr>
-    <tr class="tr_campo hide">
-        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
-            <span style="margin: auto 5px; color: black">
-                GIP
-            </span>
-        </td>
-        @foreach($semanas as $item)
-            <td class="text-center" style="border-color: #9d9d9d">
-                <div style="width: 100px">
-                    ${{number_format($item->campo_gip, 2)}}
-                </div>
-            </td>
-        @endforeach
-    </tr>
-    <tr class="tr_campo hide">
-        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
-            <span style="margin: auto 5px; color: black">
-                GA
-            </span>
-        </td>
-        @foreach($semanas as $item)
-            <td class="text-center" style="border-color: #9d9d9d">
-                <div style="width: 100px">
-                    ${{number_format($item->campo_ga, 2)}}
-                </div>
-            </td>
-        @endforeach
     </tr>
     {{-- PROPAGACIÓN --}}
     <tr>
@@ -174,6 +117,89 @@
             <td class="text-center" style="border-color: #9d9d9d">
                 <div style="width: 100px">
                     ${{number_format($item->propagacion_ga, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    {{-- CAMPO --}}
+    <tr>
+        <th class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black; font-weight: bold" class="btn btn-xs btn-link"
+                  onclick="$('.tr_campo').toggleClass('hide')">
+                CAMPO
+            </span>
+        </th>
+        @php
+            $total_campo = 0;
+        @endphp
+        @foreach($semanas as $item)
+            <th class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo, 2)}}
+                </div>
+            </th>
+            @php
+                $total_campo += $item->campo;
+            @endphp
+        @endforeach
+        <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <div style="width: 110px">
+                ${{number_format($total_campo, 2)}}
+            </div>
+        </th>
+    </tr>
+    <tr class="tr_campo hide">
+        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                MP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo_mp, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_campo hide">
+        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                MO
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo_mo, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_campo hide">
+        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GIP
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo_gip, 2)}}
+                </div>
+            </td>
+        @endforeach
+    </tr>
+    <tr class="tr_campo hide">
+        <td class="text-center th_fijo_left_0" style="background-color: #e9ecef; border-color: #9d9d9d">
+            <span style="margin: auto 5px; color: black">
+                GA
+            </span>
+        </td>
+        @foreach($semanas as $item)
+            <td class="text-center" style="border-color: #9d9d9d">
+                <div style="width: 100px">
+                    ${{number_format($item->campo_ga, 2)}}
                 </div>
             </td>
         @endforeach
