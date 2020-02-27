f81e4fa0efc0a9cc5e3e98e5b2c0accf6e85ada6
Obrian Villasmil <obrian@pyganflor.com>
2020-02-27 16:51:48 +0000

    rafa

--- resources/views/adminlte/gestion/postcocecha/clasificacion_verde/partials/rendimiento_mesas.blade.php
+++ resources/views/adminlte/gestion/postcocecha/clasificacion_verde/partials/rendimiento_mesas.blade.php
@@ -2,11 +2,20 @@
     @php
         $rend_verde = $verde != '' ? $verde->getRendimiento() : 0;
     @endphp
-    <table style="width: 100%; font-size: 1.3em; color: black" class="table-bordered" id="table_rendimiento_mesa">
+    <table style="width: 100%; font-size: 1.5em; color: black" class="table-bordered" id="table_rendimiento_mesa">
         @for($i = 1; $i<=18; $i++)
             <tr style="height: 35px">
                 @php
-                    $rend = getRendimientoVerdeByFechaMesa($fecha_verde, $i);
+                    $rend = 0;
+                    $tallos = 0;
+                    foreach($query as $item){
+                        if ($item->mesa == $i)
+                            $tallos += $item->cantidad_ramos * $item->tallos_x_ramos;
+                    }
+
+                    if ($getCantidadHorasTrabajoVerde > 0)
+                        $rend =  $tallos / $getCantidadHorasTrabajoVerde;
+
                     $color = $rend >= $rend_verde ? 'green' : 'orange';
                 @endphp
                 <th class="text-right bg-{{$color}}" style="border-color: #001F3F; width: 16%; vertical-align: middle">
@@ -14,29 +23,47 @@
                 </th>
                 <th class="text-left bg-{{$color}}"
                     style="border-color: #001F3F; width: 16%; border-right-width: 10px;">
-                    <span class="badge bg-navy-active" style="margin-left: 5px">{{round($rend, 2)}}</span>
+                    <span class="badge bg-navy-active" style="margin-left: 5px; font-size: 0.8em">{{round($rend, 2)}}</span>
                 </th>
 
                 @php
-                    $rend = getRendimientoVerdeByFechaMesa($fecha_verde, $i+18);
+                    $rend = 0;
+                    $tallos = 0;
+                    foreach($query as $item){
+                        if ($item->mesa == $i+18)
+                            $tallos += $item->cantidad_ramos * $item->tallos_x_ramos;
+                    }
+
+                    if ($getCantidadHorasTrabajoVerde > 0)
+                        $rend =  $tallos / $getCantidadHorasTrabajoVerde;
+
                     $color = $rend >= $rend_verde ? 'green' : 'orange';
                 @endphp
                 <th class="text-right bg-{{$color}}" style="border-color: #001F3F; width: 16%">
                     <span style="margin-right: 5px; color: black">{{$i+18}}</span>
                 </th>
                 <th class="text-left bg-{{$color}}" style="border-color: #001F3F; width: 16%; border-right-width: 10px;">
-                    <span class="badge bg-navy-active" style="margin-left: 5px">{{round($rend, 2)}}</span>
+                    <span class="badge bg-navy-active" style="margin-left: 5px; font-size: 0.8em">{{round($rend, 2)}}</span>
                 </th>
 
                 @php
-                    $rend = getRendimientoVerdeByFechaMesa($fecha_verde, $i+36);
+                    $rend = 0;
+                    $tallos = 0;
+                    foreach($query as $item){
+                        if ($item->mesa == $i+36)
+                            $tallos += $item->cantidad_ramos * $item->tallos_x_ramos;
+                    }
+
+                    if ($getCantidadHorasTrabajoVerde > 0)
+                        $rend =  $tallos / $getCantidadHorasTrabajoVerde;
+
                     $color = $rend >= $rend_verde ? 'green' : 'orange';
                 @endphp
                 <th class="text-right bg-{{$color}}" style="border-color: #001F3F; width: 16%">
                     <span style="margin-right: 5px; color: black">{{$i+36}}</span>
                 </th>
                 <th class="text-left bg-{{$color}}" style="border-color: #001F3F; width: 16%">
-                    <span class="badge bg-navy-active" style="margin-left: 5px">{{round($rend, 2)}}</span>
+                    <span class="badge bg-navy-active" style="margin-left: 5px; font-size: 0.8em">{{round($rend, 2)}}</span>
                 </th>
             </tr>
         @endfor
@@ -48,12 +75,12 @@
 @endif
 
 <script>
-    setInterval(function () {
+    /*setInterval(function () {
         datos = {
             fecha_verde: $('#fecha_verde_search').val().trim(),
         };
         $.get('{{url('clasificacion_verde/rendimiento_mesas')}}', datos, function (retorno) {
             $('#div_modal-modal-view_rendimiento_mesas').html(retorno);
         });
-    }, 10000)
+    }, 60000)*/
 </script>
