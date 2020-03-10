ab326ccb87c3fa098795291bf952e37dec4a8f80
Rafael <prats@pyganflor.com>
2020-03-10 14:47:27 -0500

    rafa

---
+++ app/Console/Commands/RecalcularCurvas.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace yura\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class RecalcularCurvas extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'curva_cosecha:recalcular';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando para recalcular las curvas de cosecha';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $semana_pasada = getSemanaByDate(opDiasFecha('-', 7, date('Y-m-d')));
+        $ciclos = DB::table('proyeccion_modulo_semana')
+            ->select('modelo')->distinct()
+            ->where('estado', 1)
+            ->where('tabla', 'C')
+            ->where('semana', $semana_pasada->codigo)
+            ->where('id_variedad', 3)
+            ->where('cosechados', '>', 0)
+            ->get();
+        dd($ciclos);
+    }
+}
