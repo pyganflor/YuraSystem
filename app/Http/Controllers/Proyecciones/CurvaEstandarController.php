4113112af083bf92749b4564a7f583a7be6e0223
Rafael <prats@pyganflor.com>
2020-03-23 09:29:24 -0500

    rafa

--- app/Http/Controllers/Proyecciones/CurvaEstandarController.php
+++ app/Http/Controllers/Proyecciones/CurvaEstandarController.php
@@ -25,16 +25,15 @@ class CurvaEstandarController extends Controller
         $sem_pasada = getSemanaByDate(opDiasFecha('-', 7, date('Y-m-d')));
         $query = Ciclo::All()
             ->where('estado', 1)
+            ->where('activo', 0)
             ->where('id_variedad', $request->variedad)
             //->where('poda_siembra', $request->poda_siembra)
-            ->where('fecha_inicio', '<=', $sem_desde->fecha_inicial)
-            ->where('fecha_fin', '>=', $sem_desde->fecha_inicial)
+            ->where('fecha_fin', '<=', $sem_desde->fecha_inicial)
+            ->where('fecha_fin', '>=', $sem_pasada->fecha_final)
             ->sortByDesc('fecha_inicio');
         $ciclos = [];
         foreach ($query as $item) {
             $sem_curva = getSemanaByDate(opDiasFecha('+', ($item->semana_poda_siembra * 7), $item->fecha_inicio));
-            if ($item->modulo == '90A')
-                dd($sem_curva->codigo);
             if ($sem_curva->codigo >= $sem_desde->codigo && $sem_curva->codigo <= $sem_pasada->codigo) {
                 array_push($ciclos, $item);
             }
