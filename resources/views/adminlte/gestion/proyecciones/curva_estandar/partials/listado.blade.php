f67c72d89a785ece4cdd84ede81b78e6c6a374e8
Rafael <prats@pyganflor.com>
2020-03-31 13:22:00 -0500

    rafa

--- resources/views/adminlte/gestion/proyecciones/curva_estandar/partials/listado.blade.php
+++ resources/views/adminlte/gestion/proyecciones/curva_estandar/partials/listado.blade.php
@@ -1,17 +1,33 @@
 <table class="table-striped table-bordered" style="width: 100%; border: 2px solid #9d9d9d">
     <tr>
-        <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
+        <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef" rowspan="2">
             Módulo
         </th>
-        <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
+        <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef" rowspan="2">
             Semana P/S
         </th>
-        <th class="text-center hidden" style="border-color: #9d9d9d; background-color: #e9ecef">
+        <th class="text-center hidden" style="border-color: #9d9d9d; background-color: #e9ecef" rowspan="2">
             Curva
         </th>
+        @php
+            $array_prom = [];
+        @endphp
         @for($i = $min_dia; $i <= $max_dia; $i++)
             <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
                 {{$i}}
+            </th>
+            @php
+                array_push($array_prom, [
+                    'valor' => 0,
+                    'positivos' => 0,
+                ]);
+            @endphp
+        @endfor
+    </tr>
+    <tr>
+        @for($i = $min_dia, $pos = 1; $i <= $max_dia; $i++, $pos++)
+            <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
+                {{$pos}}º
             </th>
         @endfor
     </tr>
@@ -29,17 +45,24 @@
             <th class="text-center hidden" style="border-color: #9d9d9d">
                 {{$c['ciclo']->curva}}
             </th>
-            @for($i = $min_dia; $i <= $max_dia; $i++)
+            @for($i = $min_dia, $pos = 0; $i <= $max_dia; $i++, $pos++)
                 @php
                     $exist = false;
                 @endphp
                 @foreach($c['cosechas'] as $v)
                     @if(explode('º', $v->info)[0] == $i)
+                        @php
+                            $porcent = round(($v->cosechados * 100) / $c['total_cosechado']);
+                        @endphp
                         <th class="text-center" style="border-color: #9d9d9d">
-                            {{round(($v->cosechados * 100) / $c['total_cosechado'])}}%
+                            {{$porcent}}
                         </th>
                         @php
                             $exist = true;
+                            if ($porcent > 0){
+                                $array_prom[$pos]['valor'] += $porcent;
+                                $array_prom[$pos]['positivos'] ++;
+                            }
                         @endphp
                     @endif
                 @endforeach
@@ -50,4 +73,14 @@
             @endfor
         </tr>
     @endforeach
+    <tr>
+        <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef" rowspan="1" colspan="2">
+            Promedio
+        </th>
+        @foreach($array_prom as $pos => $v)
+            <th class="text-center" style="border-color: #9d9d9d; background-color: #e9ecef">
+                {{round($v['valor'] / $v['positivos'])}}%
+            </th>
+        @endforeach
+    </tr>
 </table>
