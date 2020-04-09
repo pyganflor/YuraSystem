3d72c37adec5952c0dae770c36abbd035f126e92
Rafael <prats@pyganflor.com>
2020-04-09 13:55:02 -0500

    rafa

--- resources/views/adminlte/gestion/proyecciones/temperaturas/partials/listado.blade.php
+++ resources/views/adminlte/gestion/proyecciones/temperaturas/partials/listado.blade.php
@@ -15,18 +15,16 @@
             $modulo = $c->modulo;
             $semana = $c->semana();
         @endphp
-        @if($modulo->sector == $sector || $sector == 'T')
-            <tr>
-                <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
-                    {{$modulo->nombre}}
-                </th>
-                <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
-                    {{$semana->codigo}}
-                </th>
-                <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
-                    {{intval(difFechas($c->fecha_inicio, date('Y-m-d'))->days / 7)}}
-                </th>
-            </tr>
-        @endif
+        <tr class="{{$modulo->id_sector == $sector || $sector == 'T' ? '' : 'hide'}}">
+            <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+                {{$modulo->nombre}}
+            </th>
+            <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+                {{$semana->codigo}}
+            </th>
+            <th class="text-center" style="background-color: #e9ecef; border-color: #9d9d9d">
+                {{intval(difFechas($c->fecha_inicio, date('Y-m-d'))->days / 7)}}
+            </th>
+        </tr>
     @endforeach
 </table>
