20e81a79fc5365eaa418dcd85bbcb904ada5a564
Rafael <prats@pyganflor.com>
2020-05-07 03:50:53 -0500

    rafa

--- resources/views/adminlte/partials/dashboard_x_variedad.blade.php
+++ resources/views/adminlte/partials/dashboard_x_variedad.blade.php
@@ -272,8 +272,8 @@
                 </div>
             </div>
         </td>
-        <td colspan="2"></td>
-        <td colspan="3" class="text-center">
+        <td colspan="1"></td>
+        <td colspan="4" class="text-center">
             <div style="" class="td-org">
                 <div class="row">
                     <div class="col-md-12" style="margin-top: 10px">
