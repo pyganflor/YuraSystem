f231cfb7d5958aad0bbfd7eb46ca381daa972a25
Rafael <prats@pyganflor.com>
2020-05-11 19:15:47 -0500

    rafa

--- resources/views/adminlte/crm/fenograma_ejecucion/partials/filtrar_ciclos.blade.php
+++ resources/views/adminlte/crm/fenograma_ejecucion/partials/filtrar_ciclos.blade.php
@@ -1,60 +1,61 @@
 @if(count($ciclos) > 0)
     <div id="div_content_fixed">
-        <table {{--data-order='[[ 3, "desc" ]]'--}} class="table-striped table-bordered" width="100%" style="border: 2px solid #9d9d9d"
+        <table {{--data-order='[[ 3, "desc" ]]'--}} class="table-striped table-bordered" width="100%"
+               style="border: 1px solid #9d9d9d; border-radius: 18px 18px 0 0;"
                id="table_fenograma_ejecucion">
             <thead>
-            <tr style="background-color: #357ca5; color: white">
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d;">
+            <tr style="color: white">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px; border-radius: 18px 0 0 0">
                     Módulo
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d" width="95px">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px" width="95px">
                     Inicio
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d; width: 30px">
+                <th class="fila_fija1" style="border-color: #9d9d9d; width: 30px; padding-left: 5px">
                     Semana
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     P/S
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     Días
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     Área m<sup>2</sup>
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     Total x Semana m<sup>2</sup>
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     1ra Flor
                 </th>
-                <th class="text-center fila_fija2" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th class="fila_fija1" style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     %<sup>M</sup>
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     Tallos Cosechados
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     Real <br>
                     Tallos/m<sup>2</sup>
                 </th>
-                <th class="text-center fila_fija1" style="border-color: #9d9d9d">
+                <th class="fila_fija1" style="border-color: #9d9d9d; padding-left: 5px">
                     Cosechado <sup>%</sup>
                 </th>
-                <th class="text-center fila_fija2" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th class="fila_fija1" style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     Proy <br>
                     Tallos/m<sup>2</sup>
                 </th>
-                <th class="text-center fila_fija2" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th class="fila_fija1" style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     Ptas Iniciales
                 </th>
-                <th class="text-center fila_fija2" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th class="fila_fija1" style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     Ptas Actuales
                 </th>
-                <th class="text-center fila_fija2" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th class="fila_fija1" style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     Dend P.Ini/m<sup>2</sup>
                 </th>
-                <th class="text-center fila_fija2" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th class="fila_fija1" style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px; border-radius: 0 18px 0 0">
                     Conteo T/P
                 </th>
             </tr>
@@ -103,31 +104,31 @@
                     $tallos_m2_cos = round($tallos_cosechados / $item->area, 2);
                     $tallos_m2_proy = round((($item->plantas_actuales() * $conteo) * ((100 - $desecho) / 100)) / $item->area, 2);
                 @endphp
-                <tr style="font-size: 0.8em" onmouseover="$(this).addClass('bg-teal-active')"
-                    onmouseleave="$(this).removeClass('bg-teal-active')">
-                    <td class="text-center" style="border-color: #9d9d9d">
+                <tr style="font-size: 0.8em" onmouseover="$(this).css('background-color','#e5f7f3 !important');"
+                    onmouseleave="$(this).css('background-color','');">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{$item->modulo->nombre}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{$item->fecha_inicio}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d;">
+                    <td style="border-color: #9d9d9d;; padding-left: 5px">
                         {{$semana->codigo}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{$poda_siembra}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d;">
+                    <td style="border-color: #9d9d9d;; padding-left: 5px">
                         @if($item->fecha_fin != '')
                             {{difFechas($item->fecha_fin, $item->fecha_inicio)->days}}
                         @else
                             {{difFechas(date('Y-m-d'), $item->fecha_inicio)->days}}
                         @endif
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d;">
+                    <td style="border-color: #9d9d9d;; padding-left: 5px">
                         {{number_format($item->area, 2)}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         @php
                             if($codigo_semana == $semana->codigo){
                                 $area += $item->area;
@@ -145,12 +146,12 @@
                             }
                         @endphp
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         @if($item->fecha_cosecha != '')
                             {{difFechas($item->fecha_cosecha, $item->fecha_inicio)->days}}
                         @endif
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         @php
                             $mortalidad = $item->getMortalidad();
 
@@ -162,35 +163,35 @@
                         @endphp
                         <span style="color: {{$color}}">{{$mortalidad}}</span>
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{number_format($tallos_cosechados)}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{$tallos_m2_cos}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{$tallos_m2_proy > 0 ? round(($tallos_m2_cos / $tallos_m2_proy) * 100, 2) : 0}}%
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         @php
-                            $color = 'orange';
+                            $color = '#EF6E11';
                             if ($tallos_m2_proy < 35)
-                                $color = 'red';
+                                $color = '#D01C62';
                             if ($tallos_m2_proy > 45)
-                                $color = 'green';
+                                $color = '#00B388';
                         @endphp
                         <span style="color: {{$color}}">{{$tallos_m2_proy}}</span>
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{number_format($item->plantas_iniciales)}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{number_format($item->plantas_actuales())}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d">
+                    <td style="border-color: #9d9d9d; padding-left: 5px">
                         {{$item->getDensidadIniciales()}}
                     </td>
-                    <td class="text-center" style="border-color: #9d9d9d"
+                    <td style="border-color: #9d9d9d; padding-left: 5px"
                         title="{{$item->conteo <= 0 ? 'semana':''}}">
                         {{$conteo}}
                     </td>
@@ -220,54 +221,54 @@
                 @endphp
             @endforeach
             </tbody>
-            <tr style="background-color: #357ca5; color: white">
-                <th class="text-center" colspan="4" style="border-color: #9d9d9d">
+            <tr style="background-color: #00B388; color: white">
+                <th colspan="4" style="border-color: #9d9d9d; padding-left: 5px">
                     Totales
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                     {{count($ciclos) > 0 ? round($ciclo / count($ciclos), 2) : 0}}
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                     {{number_format(round($total_area/ 10000, 2), 2)}}
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     @if($total_mortalidad['positivos'] > 0)
                         {{round($total_mortalidad['valor'] / $total_mortalidad['positivos'], 2)}}
                     @endif
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                     {{number_format($total_tallos, 2)}}
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                     @if($positivos_tallos_m2 > 0)
                         {{count($ciclos) > 0 ? round($total_tallos_m2 / $positivos_tallos_m2, 2) : 0}}
                     @else
                         0
                     @endif
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d">
+                <th style="border-color: #9d9d9d; padding-left: 5px">
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     @if($total_tallos_m2_proy['positivos'] > 0)
                         {{round($total_tallos_m2_proy['valor'] / $total_tallos_m2_proy['positivos'], 2)}}
                     @endif
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     {{number_format($total_iniciales)}}
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     {{number_format($total_actuales)}}
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
                     @if($total_densidad['positivos'] > 0)
                         {{round($total_densidad['valor'] / $total_densidad['positivos'], 2)}}
                     @endif
                 </th>
-                <th class="text-center" style="border-color: #9d9d9d; background-color: #0b3248">
+                <th style="border-color: #9d9d9d; background-color: #00B388; padding-left: 5px">
 
                 </th>
             </tr>
@@ -276,6 +277,8 @@
 
     <script>
         estructura_tabla('table_fenograma_ejecucion', false, false);
+        $('#table_fenograma_ejecucion_filter label').addClass('text-color_yura');
+        $('#table_fenograma_ejecucion_filter label input').addClass('input-yura_default');
     </script>
 
     <style>
@@ -296,7 +299,7 @@
         }
 
         #table_fenograma_ejecucion thead .fila_fija1 {
-            background-color: #357ca5 !important;
+            background-color: #00B388 !important;
             border: 1px solid #9d9d9d !important;
             z-index: 9;
             position: sticky;
