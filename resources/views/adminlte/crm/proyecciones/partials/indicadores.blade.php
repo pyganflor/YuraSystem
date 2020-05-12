c20aed451500160d90a7df1c150fa28dc9b6bff0
Rafael <prats@pyganflor.com>
2020-05-12 17:23:23 -0500

    rafa

--- resources/views/adminlte/crm/proyecciones/partials/indicadores.blade.php
+++ resources/views/adminlte/crm/proyecciones/partials/indicadores.blade.php
@@ -1,3 +1,15 @@
+<div class="form-row">
+    <div class="col-md-4">
+        <div class="div_indicadores border-radius_16" style="background-color: #30BBBB;">
+            <legend class="text-white" style="font-size: 1.1em">Cosechado <sup>+4 semanas</sup></legend>
+            <p>Cajas: <strong class="pull-right">{{number_format($indicador[0]->valor,2,",",".")}}</strong></p>
+        </div>
+    </div>
+    <div class="col-md-4">c</div>
+    <div class="col-md-4">c</div>
+</div>
+
+
 <div class="row">
     <div class="col-md-4" style="cursor:pointer" onclick="modal_indicador('cosecha')">
         <div class="small-box bg-teal-active">
@@ -10,7 +22,7 @@
             <div class="icon">
                 <i class="fa fa-line-chart"></i>
             </div>
-            <a href="javascript:void(0)" class="small-box-footer" >
+            <a href="javascript:void(0)" class="small-box-footer">
                 Cosechado <sup>+4 semanas</sup> <i class="fa fa-arrow-circle-right"></i>
             </a>
         </div>
@@ -39,9 +51,12 @@
                         $indicardor4 = explode("|",$indicador[4]->valor);
 
                     @endphp
-                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[0])[0]}}: ${{number_format((explode(":",$indicardor4[0])[1]),2,",",".")}}</b></li>
-                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[1])[0]}}:  ${{number_format((explode(":",$indicardor4[1])[1]),2,",",".")}}</b></li>
-                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[2])[0]}}:  ${{number_format((explode(":",$indicardor4[2])[1]),2,",",".")}}</b></li>
+                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[0])[0]}}:
+                            ${{number_format((explode(":",$indicardor4[0])[1]),2,",",".")}}</b></li>
+                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[1])[0]}}:
+                            ${{number_format((explode(":",$indicardor4[1])[1]),2,",",".")}}</b></li>
+                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[2])[0]}}:
+                            ${{number_format((explode(":",$indicardor4[2])[1]),2,",",".")}}</b></li>
                 </ul>
             </div>
             <div class="icon">
@@ -54,10 +69,11 @@
     </div>
 </div>
 <style>
-    .icon{
-        font-size: 65px!important;
+    .icon {
+        font-size: 65px !important;
     }
-    .icon:hover{
-        font-size: 70px!important;
+
+    .icon:hover {
+        font-size: 70px !important;
     }
 </style>
