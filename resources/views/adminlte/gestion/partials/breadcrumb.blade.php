a31bf2fed8933b46cda5992afb9597c0b61b3374
Rafael <prats@pyganflor.com>
2020-05-13 15:29:56 -0500

    rafa

--- resources/views/adminlte/gestion/partials/breadcrumb.blade.php
+++ resources/views/adminlte/gestion/partials/breadcrumb.blade.php
@@ -1,20 +1,20 @@
 <section class="content-header">
-    <h1>
+    <h1 class="text-color_yura">
         {{$text['titulo']}}
         <small>{{$text['subtitulo']}}</small>
     </h1>
 
     <ol class="breadcrumb">
-        <li><a href="javascript:void(0)" onclick="cargar_url('')"><i class="fa fa-home"></i> Inicio</a></li>
-        <li>
+        <li><a href="javascript:void(0)" class="text-color_yura" onclick="cargar_url('')"><i class="fa fa-home"></i> Inicio</a></li>
+        <li class="text-color_yura">
             {{$submenu->menu->grupo_menu->nombre}}
         </li>
-        <li>
+        <li class="text-color_yura">
             {{$submenu->menu->nombre}}
         </li>
 
         <li class="active">
-            <a href="javascript:void(0)" onclick="cargar_url('{{$submenu->url}}')">
+            <a href="javascript:void(0)" class="text-color_yura" onclick="cargar_url('{{$submenu->url}}')">
                 <i class="fa fa-fw fa-refresh"></i> {{$submenu->nombre}}
             </a>
         </li>
