f60551e5371bbde265027711a85f310c5e7a8cc8
Rafael <prats@pyganflor.com>
2020-05-13 11:46:18 -0500

    rafa

--- resources/views/adminlte/crm/proyecciones/inicio.blade.php
+++ resources/views/adminlte/crm/proyecciones/inicio.blade.php
@@ -10,85 +10,117 @@
         <div id="div_indicadores">
             @include('adminlte.crm.proyecciones.partials.indicadores')
         </div>
-        <div class="box box-success">
-            <div class="box-header with-border">
-                <h4 class="box-title">
-                    <strong>Gráficas</strong>
-                </h4>
-                <div class="input-group">
-                    <div class="input-group-addon bg-gray">
-                        <i class="fa fa-calendar-check-o"></i> Rango
+
+        <h4 class="box-title">
+            <strong>Gráficas</strong>
+        </h4>
+        <div style="background-color: white; padding: 10px">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group input-group">
+                        <span class="input-group-addon bg-yura_dark span-input-group-yura-fixed">
+                            <i class="fa fa-fw fa-calendar-check-o"></i> Rango
+                        </span>
+                        <select name="filtro_predeterminado_rango" id="filtro_predeterminado_rango" class="form-control input-yura_default"
+                                onchange="chart_inicio()">
+                            <option value="3">3 Meses</option>
+                            <option value="6">6 Meses</option>
+                            <option value="12">1 Año</option>
+                        </select>
                     </div>
-                    <select name="filtro_predeterminado_rango" id="filtro_predeterminado_rango" class="form-control"
-                            onchange="chart_inicio()">
-                        <option value="3">3 Meses</option>
-                        <option value="6">6 Meses</option>
-                        <option value="12">1 Año</option>
-                    </select>
-                    <div class="input-group-addon bg-gray">
-                        <i class="fa fa-fw fa-leaf"></i> Variedad
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group input-group">
+                        <span class="input-group-addon bg-yura_dark span-input-group-yura-fixed">
+                            <i class="fa fa-fw fa-leaf"></i> Variedad
+                        </span>
+                        <select name="filtro_predeterminado_planta" id="filtro_predeterminado_planta" class="form-control input-yura_default"
+                                onchange="select_planta($(this).val(), 'filtro_predeterminado_variedad', 'div_cargar_variedades', '<option value=T selected>Todos los tipos</option>')">
+                            <option value="">Todas las variedades</option>
+                            @foreach(getPlantas() as $p)
+                                <option value="{{$p->id_planta}}">{{$p->nombre}}</option>
+                            @endforeach
+                        </select>
                     </div>
-                    <select name="filtro_predeterminado_planta" id="filtro_predeterminado_planta" class="form-control"
-                            onchange="select_planta($(this).val(), 'filtro_predeterminado_variedad', 'div_cargar_variedades', '<option value=T selected>Todos los tipos</option>')">
-                        <option value="">Todas las variedades</option>
-                        @foreach(getPlantas() as $p)
-                            <option value="{{$p->id_planta}}">{{$p->nombre}}</option>
-                        @endforeach
-                    </select>
-                    <div class="input-group-addon bg-gray" id="div_cargar_variedades">
-                        <i class="fa fa-fw fa-leaf"></i> Tipo
-                    </div>
-                    <select name="filtro_predeterminado_variedad" id="filtro_predeterminado_variedad" class="form-control"
-                            onchange="chart_inicio()">
-                        <option value="">Todos los tipos</option>
-                    </select>
-                    <div class="input-group-btn">
-                        <button type="button" id="btn_filtrar" class="btn btn-default" onclick="chart_inicio()" title="Buscar">
-                            <i class="fa fa-fw fa-search"></i>
-                        </button>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group input-group">
+                        <span class="input-group-addon bg-yura_dark span-input-group-yura-fixed">
+                            <i class="fa fa-fw fa-leaf"></i> Tipo
+                        </span>
+                        <select name="filtro_predeterminado_variedad" id="filtro_predeterminado_variedad" class="form-control input-yura_default"
+                                onchange="chart_inicio()">
+                            <option value="">Todos los tipos</option>
+                        </select>
+                        <div class="input-group-btn">
+                            <button type="button" id="btn_filtrar" class="btn btn-yura_dark" onclick="chart_inicio()" title="Buscar">
+                                <i class="fa fa-fw fa-search"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-md-9" id="chart_inicio">
-                        <ul class="nav nav-pills nav-justified">
-                            <li class="active"><a href="#tab_inicio_1" data-toggle="tab" aria-expanded="true"><i class="fa fa-usd"></i>Dinero</a></li>
-                            <li class=""><a href="#tab_inicio_2" data-toggle="tab" aria-expanded="false"><i class="fa fa-cubes" ></i> Cajas</a></li>
-                        </ul>
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="tab_inicio_1">
-                                <canvas id="chart_inicio_1" style="margin-top: 5px" height="100px"></canvas>
-                            </div>
-                            <div class="tab-pane" id="tab_inicio_2">
-                                <canvas id="chart_inicio_2" style="margin-top: 5px" height="100px"></canvas>
-                            </div>
+
+            <div class="row">
+                <div class="col-md-9" id="chart_inicio">
+                    <ul class="nav nav-pills nav-justified">
+                        <li class="active">
+                            <a href="#tab_inicio_1" class="border-radius_18" data-toggle="tab" aria-expanded="true">
+                                <i class="fa fa-usd"></i>Dinero
+                            </a>
+                        </li>
+                        <li class="">
+                            <a href="#tab_inicio_2" class="border-radius_18" data-toggle="tab" aria-expanded="false">
+                                <i class="fa fa-cubes"></i> Cajas
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tab_inicio_1">
+                            <canvas id="chart_inicio_1" style="margin-top: 5px" height="100px"></canvas>
+                        </div>
+                        <div class="tab-pane" id="tab_inicio_2">
+                            <canvas id="chart_inicio_2" style="margin-top: 5px" height="100px"></canvas>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="box box-solid box-info">
-                            <div class="box-header with-border ">
-                                <i class="fa fa-pie-chart"></i>
-                                <h3 class="box-title">Semana: {{$semana}}</h3>
-                            </div>
-                            <!-- /.box-header -->
-                            <div class="box-body">
-                                <div class="box-footer no-padding">
-                                    <ul class="nav nav-stacked">
-                                        <li><a href="#"><b>Tallos cosechados</b> <span class="pull-right badge bg-blue">{{number_format($indicador[5]->valor,2,",",".")}}</span></a></li>
-                                        <li><a href="#"><b>Cajas cosechadas</b> <span class="pull-right badge bg-aqua">{{number_format($indicador[7]->valor,2,",",".")}}</span></a></li>
-                                        <li><a href="#"><b>Cajas vendidas</b> <span class="pull-right badge bg-lime-active">{{number_format($indicador[6]->valor,2,",",".")}}</span></a></li>
-                                        <li><a href="#"><b>Dinero generado</b> <span class="pull-right badge bg-green">${{number_format($indicador[8]->valor,2,",",".")}}</span></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <!-- /.box-body -->
+                </div>
+                <div class="col-md-3">
+                    <div class="box box-solid box-success">
+                        <div class="box-header with-border ">
+                            <i class="fa fa-pie-chart"></i>
+                            <h3 class="box-title">Semana: {{$semana}}</h3>
                         </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="box-footer no-padding">
+                                <ul class="nav nav-stacked">
+                                    <li><a href="#"><b>Tallos cosechados</b> <span
+                                                    class="pull-right badge bg-blue">{{number_format($indicador[5]->valor,2,",",".")}}</span></a>
+                                    </li>
+                                    <li><a href="#"><b>Cajas cosechadas</b> <span
+                                                    class="pull-right badge bg-aqua">{{number_format($indicador[7]->valor,2,",",".")}}</span></a>
+                                    </li>
+                                    <li><a href="#"><b>Cajas vendidas</b> <span
+                                                    class="pull-right badge bg-lime-active">{{number_format($indicador[6]->valor,2,",",".")}}</span></a>
+                                    </li>
+                                    <li><a href="#"><b>Dinero generado</b> <span
+                                                    class="pull-right badge bg-green">${{number_format($indicador[8]->valor,2,",",".")}}</span></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
                     </div>
                 </div>
             </div>
         </div>
     </section>
+@endsection
+
+@section('css_final')
+    <style>
+
+    </style>
 @endsection
 
 @section('script_final')
--- resources/views/adminlte/crm/proyecciones/partials/indicadores.blade.php
+++ resources/views/adminlte/crm/proyecciones/partials/indicadores.blade.php
@@ -1,81 +1,50 @@
-<div class="form-row">
+<div class="row" style="margin-bottom: 10px">
     <div class="col-md-4">
-        <div class="div_indicadores border-radius_16" style="background-color: #30BBBB;">
-            <legend class="text-white" style="font-size: 1.1em">Cosechado <sup>+4 semanas</sup></legend>
-            <p>Cajas: <strong class="pull-right">{{number_format($indicador[0]->valor,2,",",".")}}</strong></p>
-            <p>Tallos: <strong class="pull-right">{{number_format($indicador[1]->valor,2,",",".")}}</strong></p>
-            <legend></legend>
+        <div class="div_indicadores border-radius_16" style="background-color: #30BBBB; margin-bottom: 5px">
+            <legend class="text-center" style="font-size: 1.1em; margin-bottom: 5px; color: white">Cosechado <sup>+4 semanas</sup></legend>
+            <p style="color: white">Cajas <strong class="pull-right">{{number_format($indicador[0]->valor,2,",",".")}}</strong></p>
+            <p style="color: white">Tallos <strong class="pull-right">{{number_format($indicador[1]->valor,2,",",".")}}</strong></p>
+            <legend style="margin-bottom: 5px; color: white"></legend>
+            <p class="text-center" style="margin-bottom: 0px">
+                <a href="javascript:void(0)" class="text-center" style="color: white" onclick="modal_indicador('cosecha')">
+                    <strong>Ver más <i class="fa fa-fw fa-arrow-circle-right"></i></strong>
+                </a>
+            </p>
         </div>
     </div>
-    <div class="col-md-4">c</div>
-    <div class="col-md-4">c</div>
-</div>
-
-
-<div class="row">
-    <div class="col-md-4" style="cursor:pointer" onclick="modal_indicador('cosecha')">
-        <div class="small-box bg-teal-active">
-            <div class="inner">
-                <ul class="info-box-number list-unstyled">
-                    <li><b>Cajas:</b> {{number_format($indicador[0]->valor,2,",",".")}}</li>
-                    <li><b>Tallos:</b> {{number_format($indicador[1]->valor,2,",",".")}}</li>
-                </ul>
-            </div>
-            <div class="icon">
-                <i class="fa fa-line-chart"></i>
-            </div>
-            <a href="javascript:void(0)" class="small-box-footer">
-                Cosechado <sup>+4 semanas</sup> <i class="fa fa-arrow-circle-right"></i>
-            </a>
+    <div class="col-md-4">
+        <div class="div_indicadores border-radius_16" style="background-color: #FFFFFF; margin-bottom: 5px">
+            <legend class="text-center" style="font-size: 1.1em; margin-bottom: 5px; color: #04C0EF">Proyectado <sup>+4 semanas</sup></legend>
+            <p>Cajas <strong class="pull-right">{{number_format($indicador[2]->valor,2,",",".")}}</strong></p>
+            <p>Dinero <strong class="pull-right">${{number_format($indicador[3]->valor,2,",",".")}}</strong></p>
+            <legend style="margin-bottom: 5px"></legend>
+            <p class="text-center" style="margin-bottom: 0px">
+                <a href="javascript:void(0)" class="text-center" style="color: #04C0EF" onclick="modal_indicador('venta')">
+                    <strong>Ver más <i class="fa fa-fw fa-arrow-circle-right"></i></strong>
+                </a>
+            </p>
         </div>
     </div>
-    <div class="col-md-4" style="cursor:pointer" onclick="modal_indicador('venta')">
-        <div class="small-box bg-aqua">
-            <div class="inner">
-                <ul class="info-box-number list-unstyled">
-                    <li><b>Cajas:</b> {{number_format($indicador[2]->valor,2,",",".")}}</li>
-                    <li><b>Dinero:</b> ${{number_format($indicador[3]->valor,2,",",".")}}</li>
-                </ul>
-            </div>
-            <div class="icon">
-                <i class="fa fa-fw fa-leaf"></i>
-            </div>
-            <a href="javascript:void(0)" class="small-box-footer">
-                Proyectado <sup>+4 semanas</sup> <i class="fa fa-arrow-circle-right"></i>
-            </a>
-        </div>
-    </div>
-    <div class="col-md-4" style="cursor:pointer" onclick="modal_indicador('venta a 3 meses')">
-        <div class="small-box bg-orange">
-            <div class="inner" style="padding: 3.5px 10px">
-                <ul class="info-box-number list-unstyled">
-                    @php
-                        $indicardor4 = explode("|",$indicador[4]->valor);
-
-                    @endphp
-                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[0])[0]}}:
-                            ${{number_format((explode(":",$indicardor4[0])[1]),2,",",".")}}</b></li>
-                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[1])[0]}}:
-                            ${{number_format((explode(":",$indicardor4[1])[1]),2,",",".")}}</b></li>
-                    <li style="font-size: 15px"><b>{{explode(":",$indicardor4[2])[0]}}:
-                            ${{number_format((explode(":",$indicardor4[2])[1]),2,",",".")}}</b></li>
-                </ul>
-            </div>
-            <div class="icon">
-                <i class="fa fa-calendar-check-o"></i>
-            </div>
-            <a href="javascript:void(0)" class="small-box-footer">
-                Dinero proyectado <sup>3 meses</sup> <i class="fa fa-arrow-circle-right"></i>
-            </a>
+    <div class="col-md-4">
+        <div class="div_indicadores border-radius_16" style="background-color: #FFFFFF; margin-bottom: 5px">
+            <legend class="text-center" style="font-size: 1.1em; margin-bottom: 5px; color: #EF6E11">$ Proyectado <sup>+3 meses</sup></legend>
+            <ul class="info-box-number list-unstyled">
+                @php
+                    $indicardor4 = explode("|",$indicador[4]->valor);
+                @endphp
+                <li style="font-size: 15px"><b>{{explode(":",$indicardor4[0])[0]}}:
+                        <strong class="pull-right">${{number_format((explode(":",$indicardor4[0])[1]),2,",",".")}}</strong></b></li>
+                <li style="font-size: 15px"><b>{{explode(":",$indicardor4[1])[0]}}:
+                        <strong class="pull-right">${{number_format((explode(":",$indicardor4[1])[1]),2,",",".")}}</strong></b></li>
+                <li style="font-size: 15px"><b>{{explode(":",$indicardor4[2])[0]}}:
+                        <strong class="pull-right">${{number_format((explode(":",$indicardor4[2])[1]),2,",",".")}}</strong></b></li>
+            </ul>
+            <legend style="margin-bottom: 0px"></legend>
+            <p class="text-center" style="margin-bottom: 0px">
+                <a href="javascript:void(0)" class="text-center" style="color: #EF6E11" onclick="modal_indicador('venta')">
+                    <strong>Ver más <i class="fa fa-fw fa-arrow-circle-right"></i></strong>
+                </a>
+            </p>
         </div>
     </div>
 </div>
-<style>
-    .icon {
-        font-size: 65px !important;
-    }
-
-    .icon:hover {
-        font-size: 70px !important;
-    }
-</style>
