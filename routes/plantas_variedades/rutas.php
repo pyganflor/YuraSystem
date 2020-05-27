759c9c0d94966f751486e58ec1373f22efdc9f4f
Rafael <prats@pyganflor.com>
2020-05-27 12:04:20 -0500

    rafa

--- routes/plantas_variedades/rutas.php
+++ routes/plantas_variedades/rutas.php
@@ -1,28 +1,28 @@
 <?php
 
-Route::get('plantas_variedades', 'PlantaVariedadController@inicio');
-Route::get('plantas_variedades/select_planta', 'PlantaVariedadController@select_planta');
-Route::get('plantas_variedades/add_planta', 'PlantaVariedadController@add_planta');
-Route::post('plantas_variedades/store_planta', 'PlantaVariedadController@store_planta');
-Route::get('plantas_variedades/edit_planta', 'PlantaVariedadController@edit_planta');
-Route::post('plantas_variedades/update_planta', 'PlantaVariedadController@update_planta');
-Route::post('plantas_variedades/cambiar_estado_planta', 'PlantaVariedadController@cambiar_estado_planta');
+Route::get('plantas_variedades', 'PlantaController@inicio');
+Route::get('plantas_variedades/select_planta', 'PlantaController@select_planta');
+Route::get('plantas_variedades/add_planta', 'PlantaController@add_planta');
+Route::post('plantas_variedades/store_planta', 'PlantaController@store_planta');
+Route::get('plantas_variedades/edit_planta', 'PlantaController@edit_planta');
+Route::post('plantas_variedades/update_planta', 'PlantaController@update_planta');
+Route::post('plantas_variedades/cambiar_estado_planta', 'PlantaController@cambiar_estado_planta');
 
 
-Route::get('plantas_variedades/add_variedad', 'PlantaVariedadController@add_variedad');
-Route::post('plantas_variedades/store_variedad', 'PlantaVariedadController@store_variedad');
-Route::get('plantas_variedades/edit_variedad', 'PlantaVariedadController@edit_variedad');
-Route::post('plantas_variedades/update_variedad', 'PlantaVariedadController@update_variedad');
-Route::post('plantas_variedades/cambiar_estado_variedad', 'PlantaVariedadController@cambiar_estado_variedad');
+Route::get('plantas_variedades/add_variedad', 'PlantaController@add_variedad');
+Route::post('plantas_variedades/store_variedad', 'PlantaController@store_variedad');
+Route::get('plantas_variedades/edit_variedad', 'PlantaController@edit_variedad');
+Route::post('plantas_variedades/update_variedad', 'PlantaController@update_variedad');
+Route::post('plantas_variedades/cambiar_estado_variedad', 'PlantaController@cambiar_estado_variedad');
 
 
-Route::get('plantas_variedades/form_precio_variedad','PlantaVariedadController@form_precio_variedad');
-Route::post('plantas_variedades/store_precio','PlantaVariedadController@store_precio');
-Route::post('plantas_variedades/update_precio','PlantaVariedadController@update_precio');
-Route::get('plantas_variedades/add_inptus_precio_variedad','PlantaVariedadController@add_inptus_precio_variedad');
+Route::get('plantas_variedades/form_precio_variedad','PlantaController@form_precio_variedad');
+Route::post('plantas_variedades/store_precio','PlantaController@store_precio');
+Route::post('plantas_variedades/update_precio','PlantaController@update_precio');
+Route::get('plantas_variedades/add_inptus_precio_variedad','PlantaController@add_inptus_precio_variedad');
 
-Route::get('plantas_variedades/vincular_variedad_unitaria','PlantaVariedadController@vincular_variedad_unitaria');
-Route::post('plantas_variedades/store_vinculo','PlantaVariedadController@store_vinculo');
-Route::get('plantas_variedades/add_regalias','PlantaVariedadController@add_regalias');
-Route::post('plantas_variedades/buscar_regalias','PlantaVariedadController@buscar_regalias');
-Route::post('plantas_variedades/store_regalias','PlantaVariedadController@store_regalias');
+Route::get('plantas_variedades/vincular_variedad_unitaria','PlantaController@vincular_variedad_unitaria');
+Route::post('plantas_variedades/store_vinculo','PlantaController@store_vinculo');
+Route::get('plantas_variedades/add_regalias','PlantaController@add_regalias');
+Route::post('plantas_variedades/buscar_regalias','PlantaController@buscar_regalias');
+Route::post('plantas_variedades/store_regalias','PlantaController@store_regalias');
